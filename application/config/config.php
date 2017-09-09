7203423d0a25720c27696a8166aa67cb42c4536e
suresh07 <soori_rozee@yahoo.com>
2017-09-09 12:31:14 +0530

    Project: Initial setup

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://localhost/iitm-nosql/');
+define('BASE_URL', 'http://localhost/vp-nosql/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('DATA_URL', PUBLIC_URL . 'data/');
 define('METADATA_URL', PUBLIC_URL . 'metaData/');
@@ -10,7 +10,7 @@ define('FLAT_URL', BASE_URL . 'application/views/flat/');
 define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 
 // Physical location of resources
-define('PHY_BASE_URL', '/var/www/html/iitm-nosql/');
+define('PHY_BASE_URL', '/var/www/html/vp-nosql/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_DATA_URL', PHY_PUBLIC_URL . 'data/');
 define('PHY_METADATA_URL', PHY_PUBLIC_URL . 'metaData/');
@@ -21,14 +21,14 @@ define('PHY_STOCK_IMAGE_URL', PHY_PUBLIC_URL . 'images/stock/');
 
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '27017');
-define('DB_NAME', 'iitmARCHIVES');
-define('DB_USER', 'iitmUSER');
-define('DB_PASSWORD', 'iitm123');
+define('DB_NAME', 'vpARCHIVES');
+define('DB_USER', 'vpUSER');
+define('DB_PASSWORD', 'vp123');
 
 // Git config
 define('GIT_USER_NAME', 'shruthisdst');
 define('GIT_PASSWORD', 'shruthitr14');
-define('GIT_REPO', 'github.com/SrirangaDigital/iitm-nosql.git');
+define('GIT_REPO', 'github.com/SrirangaDigital/vp-nosql.git');
 define('GIT_REMOTE', 'https://' . GIT_USER_NAME . ':' . GIT_PASSWORD . '@' . GIT_REPO);
 define('GIT_EMAIL', 'shruthitr.nayak@gmail.com');
 
--- application/config/settings.php
+++ application/config/settings.php
@@ -1,18 +1,13 @@
 <?php
 //Collections
 define('ARTEFACT_COLLECTION', 'artefacts');
-define('FOREIGN_KEY_COLLECTION', 'foreignKeys');
 define('USER_COLLECTION', 'userdetails');
 
 //Default Values
 define('SHOW_ONLY_IF_DATA_EXISTS', True);
-define('DEFAULT_TYPE', 'Letter');
-define('MISCELLANEOUS_NAME', 'Miscellaneous');
-define('FOREIGN_KEY_TYPE', 'ForeignKeyType');
 
 // Lazy loading setting
 define('PER_PAGE', 10);
-define('PHOTO_FILE_EXT', '.JPG');
 
 // user settings (login and registration)
 define('REQUIRE_EMAIL_VALIDATION', False);//Set these values to True only

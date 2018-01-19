7ab786c8a8453933dc8f0cf112cd109a3b8f3b27
suresh07 <soori_rozee@yahoo.com>
2018-01-19 17:26:17 +0530

    Project: order of article data in index.json changed

--- application/controllers/data.php
+++ application/controllers/data.php
@@ -31,12 +31,14 @@ class data extends Controller {
 				$data = array_filter($data);
 				$result = $collection->insertOne($data);
 
+				// fetching initial letter from author
 				if(isset($article['author'])) {
 
 					foreach ($article['author'] as $author) 
 					array_push($authorAlphabet, preg_replace('/(^.).*/u', '$1', $author['name']));
 				}
 
+				// fetching initial letter from title
 				array_push($titleAlphabet, preg_replace('/(^.).*/u', '$1', $article['title']));
 			}
 		}
@@ -56,6 +58,67 @@ class data extends Controller {
 		$result = $collection->insertOne($data);
 	}
 	
+	public function xml2Json() {
+
+		$xml = simplexml_load_file(BASE_URL . 'vp.xml');
+
+		foreach ($xml->volume as $volume) {
+			
+			foreach ($volume->issue as $issue) {
+				
+				$completeIssue = [];
+				$completeIssue['volume'] = (string)$volume['vnum'];
+				
+				foreach ($issue->entry as $entry) {
+
+					$completeIssue['issue'] = (string)$issue['inum'];
+					$completeIssue['year'] = (string)$issue['year'];
+					$completeIssue['month'] = (string)$issue['month'];
+					$completeIssue['theme'] = (string)$issue['theme'];
+					$completeIssue['id'] = $completeIssue['volume'] . '/' . $completeIssue['issue'];
+					
+					$array = [];
+					$array['title'] = $entry->title->__toString();
+					$array['feature'] = $entry->feature->__toString();
+					$array['page'] = $entry->page->__toString();
+					$splitPage = explode('-', $array['page']);
+
+					$jsonFilePath = PHY_METADATA_URL . $completeIssue['volume'] . '/' . $completeIssue['issue'] . '/';
+					$files = glob($jsonFilePath . "text/*.txt");
+					$articleStartOffset = array_search($jsonFilePath . 'text/' . $splitPage[0] . '.txt', $files);
+					$articleEndOffset = array_search($jsonFilePath . 'text/' . $splitPage[1] . '.txt', $files) + 1;
+					$textFiles = array_slice($files, $articleStartOffset, $articleEndOffset - $articleStartOffset);
+
+					$textArray = [];
+					$array['fullText'] = [];
+					foreach ($textFiles as $textFile) {
+
+						preg_match('/(.*)\/text\/(.*)\.txt/', $textFile, $matches);
+						$textArray['page'] = $matches[2];
+						$textArray['text'] = trim(file_get_contents($textFile));
+						array_push($array['fullText'], $textArray);
+					}
+
+					$array['relativePageNumber'] = (array_search($jsonFilePath . 'text/' . $splitPage[0] . '.txt', $files)) ? array_search($jsonFilePath . 'text/' . $splitPage[0] . '.txt', $files) : 1;
+					
+					foreach ($entry->allauthors->author as $author) {
+
+						$arrayArthor = [];
+						$arrayArthor['name'] = $author->__toString();
+						$arrayArthor['salutation'] = (string)$author['salut'];
+						$array['author'][] = $arrayArthor;
+					}
+
+					$completeIssue['toc'][] = $array;
+				}
+				
+				exec("mkdir -p " . $jsonFilePath);
+				$json = json_encode($completeIssue, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+				file_put_contents($jsonFilePath . 'index.json' , $json);
+			}
+		}
+	}
+
 	// Use this method for global changes in json files
 	public function modify() {
 

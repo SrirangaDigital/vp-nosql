ed4ef3d5a26c8462bc9cb07d2db3b731d26a2b25
suresh07 <soori_rozee@yahoo.com>
2018-04-21 13:25:57 +0530

    Home page UI fixes

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -2,68 +2,130 @@
     <div class="row justify-content-center">
         <div class="col-md-9 gap-above">
             <div class="row">
-            	<div class="col-md-6 mainArticle">
-            		<img class="img-fluid" src="<?=IMAGE_URL?>article/mainArticle.JPG">
-            		<p class="title">ಆಧುನಿಕ ಪ್ರವೃತ್ತಿ ಮತ್ತು ಸನಾತನ ತತ್ತ್ವ</p>
-            		<p class="content">ಮುಳಬಾಗಲಿನ ಆಂಜನೇಯಸ್ವಾಮಿಯ ದೇವಾಲಯ ಬಹು ಪ್ರಸಿದ್ಧವಾದದ್ದು ಮೈಸೂರು ಬೆಂಗಳೂರು ಪ್ರಾಂತಗಳಿಂದ ತಿರುಪತಿಗೆ ಯಾತ್ರೆ ಹೊರಟವರು ಪ್ರಾಂತಗಳಿಂದ...</p>
-            		<p class="author by text-right">ಸಂಪಾದಕೀಯ</p>
-            		<div class="row subArticle">
-            			<div class="col-md-6">
-            				<img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle3.JPG">
-            			</div>
-            			<div class="col-md-6">
-            				<p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು-೧೪</p>
-            				<p class="content">ಆ ವಯಸ್ಕ ದಂಪತಿಗಳೇ ಕ್ಯಾಪ್ಟನ್ ಹಾಗೂ ಮಿಸೆಸ್ ಸೇವಿಯರ್; ಸ್ವರೂಪರೇ ಸ್ವಾಮಿ ಸ್ವರೂಪಾನಂದರು ಆ ವಯಸ್ಕ ದಂಪತಿಗಳೇ ಕ್ಯಾಪ್ಟನ್ ಹಾಗೂ ಮಿಸೆಸ್ ಸೇವಿಯರ್; ಸ್ವರೂಪರೇ ಸ್ವಾಮಿ ಸ್ವರೂಪಾನಂದರು ...</p>
-            				<p class="author by text-right">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</p>
-            			</div>
-            		</div>
-            	</div>
-            	<div class="col-md-2 subArticle">
-            		<div class="row">
-            			<div class="col-md-12">
-		            		<img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle1.JPG">
-		            		<p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ನುಡಿಗಳ ತಾತ್ಪರ್ಯ</p>
-		            		<p class="content">ಯುಗಾಚಾರ್ಯ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರನ್ನು ವರ್ಣಿಸುವದ ಸೂರ್ಯನ ತೇಜಸ್ಸನ್ನು...</p>
-		            		<p class="author by text-right">ವಿಶ್ವರೂಪಾನಂದ, ಸ್ವಾಮಿ</p>
-            			</div>
-            			<div class="col-md-12">
-		            		<img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle2.JPG">
-		            		<p class="title">ಮನಸ್ಸು ಮತ್ತು ಸಾಧನೆ</p>
-		            		<p class="content">ಚಿತ್ತಶುದ್ಧಿ ಆಗುವವರೆಗೂ ಭಗವಂತನ ದರ್ಶನ ದೊರೆಯುವ ಹಾಗಿಲ್ಲಕಾಮ, ಕ್ರೋಧ, ಲೋಭ ಇವನ್ನೆಲ್ಲ...</p>
-		            		<p class="author by text-right">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
-            			</div>
-            		</div>
-            	</div>
+            	<div class="col-md-5 mainArticle">
+                        <div class="row">
+                              <div col-md-12>
+                                    <a href="javascript:void(0);">
+                                          <img class="img-fluid" src="<?=IMAGE_URL?>article/mainArticle.JPG">
+                                          <p class="title">ಆಧುನಿಕ ಪ್ರವೃತ್ತಿ ಮತ್ತು ಸನಾತನ ತತ್ತ್ವ</p>
+                                          <p class="content">ಮುಳಬಾಗಲಿನ ಆಂಜನೇಯಸ್ವಾಮಿಯ ದೇವಾಲಯ ಬಹು ಪ್ರಸಿದ್ಧವಾದದ್ದು ಮೈಸೂರು ಬೆಂಗಳೂರು ಪ್ರಾಂತಗಳಿಂದ ತಿರುಪತಿಗೆ ಯಾತ್ರೆ ಹೊರಟವರು ಪ್ರಾಂತಗಳಿಂದ...</p>
+                                          <p class="author by">ಸಂಪಾದಕೀಯ</p>
+                                    </a>
+                              </div>
+                        </div>
+                        <div class="row">
+                              <div col-md-12>
+                                    <a href="javascript:void(0);">
+                                          <img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle3.JPG">
+                                          <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು-೧೪</p>
+                                          <p class="content">ಆ ವಯಸ್ಕ ದಂಪತಿಗಳೇ ಕ್ಯಾಪ್ಟನ್ ಹಾಗೂ ಮಿಸೆಸ್ ಸೇವಿಯರ್; ಸ್ವರೂಪರೇ ಸ್ವಾಮಿ ಸ್ವರೂಪಾನಂದರು ಆ ವಯಸ್ಕ ದಂಪತಿಗಳೇ ಕ್ಯಾಪ್ಟನ್ ...</p>
+                                          <p class="author by">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</p>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-md-3 subArticle">
+                        <div class="row">
+                              <div col-md-12>
+                                    <a href="javascript:void(0);">
+                                          <img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle1.JPG">
+                                          <p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ನುಡಿಗಳ ತಾತ್ಪರ್ಯ</p>
+                                          <p class="content">ಯುಗಾಚಾರ್ಯ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರನ್ನು ವರ್ಣಿಸುವದ ಸೂರ್ಯನ ತೇಜಸ್ಸನ್ನು...</p>
+                                          <p class="author by">ವಿಶ್ವರೂಪಾನಂದ, ಸ್ವಾಮಿ</p>
+                                    </a>
+                              </div>
+                        </div>
+                        <div class="row">
+                              <div col-md-12>
+                                    <a href="javascript:void(0);">
+                                          <img class="img-fluid" src="<?=IMAGE_URL?>article/subArticle2.JPG">
+                                          <p class="title">ಮನಸ್ಸು ಮತ್ತು ಸಾಧನೆ</p>
+                                          <p class="content">ಚಿತ್ತಶುದ್ಧಿ ಆಗುವವರೆಗೂ ಭಗವಂತನ ದರ್ಶನ ದೊರೆಯುವ ಹಾಗಿಲ್ಲಕಾಮ, ಕ್ರೋಧ, ಲೋಭ ಇವನ್ನೆಲ್ಲ...</p>
+                                          <p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
             	<div class="col-md-4 cardArticle">
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle2.JPG" />
+                                    <p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೧೩</p>
+                                    <p class="content">ಡಾಕ್ಟರ್ ಕಂಜೀಲಾಲ್ ಕಲ್ಕತ್ತೆಗೆ ಹೊರಡುವ ದಿನ ಮಧ್ಯಾಹ್ನದ ಹೊತ್ತು ನನ್ನನ್ನುಹೊತ್ತು ನನ್ನನ್ನು...</p>
+                                    <p class="author by">ಆಶುತೋಷ್ ಮಿತ್ರ</p>
+                              </a>
+                        </div>
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle1.JPG"/>
+                                    <p class="title">ತನ್ಮೇ ಮನಃ ಶಿವಸಂಕಲ್ಪಮಸ್ತು</p>
+                                    <p class="content">ಶಿವನಾಗಪುರವೆಂಬ ಗ್ರಾಮದಲ್ಲಿ ತಾರಾಪ್ರಿಯನೆಂಬ ಒಬ್ಬ ದೈವಜ್ಞನಿದ್ದನು ದೈವಭಕ್ತನಾದ...</p>
+                                    <p class="author by">ವೆಂ. ಲಕ್ಷ್ಮೀಶ</p>
+                              </a>
+                        </div>
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle3.JPG">
+                                    <p class="title">ಶ್ರೀಮಧ್ವಾಚಾರ್ಯರು</p>
+                                    <p class="content">ತರು ಬಹು ದೊಡ್ಡ ಅದ್ವೈತ ವಿದ್ವಾಂಸರಾಗಿದ್ದರು ಆಚಾರ್ಯರ ಜೊತೆ ವಾದ ಮಾಡಿ ಸೋತುಹೋದ...</p>
+                                    <p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
+                        </div>
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle4.JPG">
+                                    <p class="title">ಪಂಡರಾಪುರದ ಇತಿಹಾಸ</p>
+                                    <p class="content">ಮಹಾರಾಷ್ಟ್ರದಲ್ಲಿ ಪುಂಡಲೀಕನೆಂಬುವನೊಬ್ಬನಿದ್ದ ಅವನು ತನ್ನ ಪತ್ನಿಯ ಮಾತು...</p>
+                                    <p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
+                        </div>
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle5.JPG">
+                                    <p class="title">ರಾಮಕೃಷ್ಣ ರಕ್ಷಿಸೈ</p>
+                                    <p class="content">ಚಿತ್ತಶುದ್ಧಿ ಆಗುವವರೆಗೂ ಭಗವಂತನ ದರ್ಶನ ದೊರೆಯುವ ಹಾಗಿಲ್ಲಕಾಮ, ಕ್ರೋಧ,...</p>
+                                    <p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
+                        </div>
+                        <div class="cards-wrapper">
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle2.JPG" />
+                                    <p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೧೩</p>
+                                    <p class="content">ಡಾಕ್ಟರ್ ಕಂಜೀಲಾಲ್ ಕಲ್ಕತ್ತೆಗೆ ಹೊರಡುವ ದಿನ ಮಧ್ಯಾಹ್ನದ ಹೊತ್ತು ನನ್ನನ್ನುಹೊತ್ತು ನನ್ನನ್ನು...</p>
+                                    <p class="author by">ಆಶುತೋಷ್ ಮಿತ್ರ</p>
+                              </a>
+                        </div>
             		<div class="cards-wrapper">
-            			<img src="<?=IMAGE_URL?>article/cardArticle1.JPG" />
-            			<p class="title">ತನ್ಮೇ ಮನಃ ಶಿವಸಂಕಲ್ಪಮಸ್ತು</p>
-            			<p class="content">ಶಿವನಾಗಪುರವೆಂಬ ಗ್ರಾಮದಲ್ಲಿ ತಾರಾಪ್ರಿಯನೆಂಬ ಒಬ್ಬ ದೈವಜ್ಞನಿದ್ದನು ದೈವಭಕ್ತನಾದ...</p>
-            			<p class="author by text-right">ವೆಂ. ಲಕ್ಷ್ಮೀಶ</p>
+                              <a href="javascript:void(0);">
+                                    <img src="<?=IMAGE_URL?>article/cardArticle1.JPG"/>
+                              	<p class="title">ತನ್ಮೇ ಮನಃ ಶಿವಸಂಕಲ್ಪಮಸ್ತು</p>
+                  			<p class="content">ಶಿವನಾಗಪುರವೆಂಬ ಗ್ರಾಮದಲ್ಲಿ ತಾರಾಪ್ರಿಯನೆಂಬ ಒಬ್ಬ ದೈವಜ್ಞನಿದ್ದನು ದೈವಭಕ್ತನಾದ...</p>
+                  			<p class="author by">ವೆಂ. ಲಕ್ಷ್ಮೀಶ</p>
+                              </a>
             		</div>
             		<div class="cards-wrapper">
-            			<img src="<?=IMAGE_URL?>article/cardArticle2.JPG" />
-            			<p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೧೩</p>
-            			<p class="content">ಡಾಕ್ಟರ್ ಕಂಜೀಲಾಲ್ ಕಲ್ಕತ್ತೆಗೆ ಹೊರಡುವ ದಿನ ಮಧ್ಯಾಹ್ನದ ಹೊತ್ತು ನನ್ನನ್ನುಹೊತ್ತು ನನ್ನನ್ನು...</p>
-            			<p class="author by text-right">ಆಶುತೋಷ್ ಮಿತ್ರ</p>
+                              <a href="javascript:void(0);">
+                  			<img src="<?=IMAGE_URL?>article/cardArticle3.JPG">
+                  			<p class="title">ಶ್ರೀಮಧ್ವಾಚಾರ್ಯರು</p>
+                  			<p class="content">ತರು ಬಹು ದೊಡ್ಡ ಅದ್ವೈತ ವಿದ್ವಾಂಸರಾಗಿದ್ದರು ಆಚಾರ್ಯರ ಜೊತೆ ವಾದ ಮಾಡಿ ಸೋತುಹೋದ...</p>
+                  			<p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
             		</div>
             		<div class="cards-wrapper">
-            			<img src="<?=IMAGE_URL?>article/cardArticle3.JPG">
-            			<p class="title">ಶ್ರೀಮಧ್ವಾಚಾರ್ಯರು</p>
-            			<p class="content">ತರು ಬಹು ದೊಡ್ಡ ಅದ್ವೈತ ವಿದ್ವಾಂಸರಾಗಿದ್ದರು ಆಚಾರ್ಯರ ಜೊತೆ ವಾದ ಮಾಡಿ ಸೋತುಹೋದ...</p>
-            			<p class="author by text-right">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              <a href="javascript:void(0);">
+                  			<img src="<?=IMAGE_URL?>article/cardArticle4.JPG">
+                  			<p class="title">ಪಂಡರಾಪುರದ ಇತಿಹಾಸ</p>
+                  			<p class="content">ಮಹಾರಾಷ್ಟ್ರದಲ್ಲಿ ಪುಂಡಲೀಕನೆಂಬುವನೊಬ್ಬನಿದ್ದ ಅವನು ತನ್ನ ಪತ್ನಿಯ ಮಾತು...</p>
+                  			<p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
             		</div>
             		<div class="cards-wrapper">
-            			<img src="<?=IMAGE_URL?>article/cardArticle4.JPG">
-            			<p class="title">ಪಂಡರಾಪುರದ ಇತಿಹಾಸ</p>
-            			<p class="content">ಮಹಾರಾಷ್ಟ್ರದಲ್ಲಿ ಪುಂಡಲೀಕನೆಂಬುವನೊಬ್ಬನಿದ್ದ ಅವನು ತನ್ನ ಪತ್ನಿಯ ಮಾತು...</p>
-            			<p class="author by text-right">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
-            		</div>
-            		<div class="cards-wrapper">
-            			<img src="<?=IMAGE_URL?>article/cardArticle5.JPG">
-            			<p class="title">ರಾಮಕೃಷ್ಣ ರಕ್ಷಿಸೈ</p>
-            			<p class="content">ಚಿತ್ತಶುದ್ಧಿ ಆಗುವವರೆಗೂ ಭಗವಂತನ ದರ್ಶನ ದೊರೆಯುವ ಹಾಗಿಲ್ಲಕಾಮ, ಕ್ರೋಧ,...</p>
-            			<p class="author by text-right">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              <a href="javascript:void(0);">
+                  			<img src="<?=IMAGE_URL?>article/cardArticle5.JPG">
+                  			<p class="title">ರಾಮಕೃಷ್ಣ ರಕ್ಷಿಸೈ</p>
+                  			<p class="content">ಚಿತ್ತಶುದ್ಧಿ ಆಗುವವರೆಗೂ ಭಗವಂತನ ದರ್ಶನ ದೊರೆಯುವ ಹಾಗಿಲ್ಲಕಾಮ, ಕ್ರೋಧ,...</p>
+                  			<p class="author by">ಶ್ರೀರಾಮಕೃಷ್ಣ ವಚನವೇದದಿಂದ</p>
+                              </a>
             		</div>
             	</div>
             </div>

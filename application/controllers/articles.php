41eefe5b2e006334d055760362879c3d68b64bd1
suresh07 <soori_rozee@yahoo.com>
2018-04-23 13:40:35 +0530

    Redirecting article page for new view

--- application/controllers/articles.php
+++ application/controllers/articles.php
@@ -39,7 +39,7 @@ class articles extends Controller {
 		$url = BASE_URL . 'api/articles?' . $filter;
 		$result = json_decode($this->model->getDataFromApi($url), true);
 		$result['pageTitle'] = ARCHIVE . ' > ' . TOC . ' > ' . ARCHIVE_VOLUME . ' ' . $viewHelper->roman2Kannada($viewHelper->rlZero($query['volume'])) . ', ' . ARCHIVE_ISSUE . ' '. $viewHelper->roman2Kannada($viewHelper->rlZero($query['issue']));
-		($result) ? $this->view('articles/articles', json_encode($result)) : $this->view('error/index');
+		($query['volume'] >= '019' AND $query['issue'] >= '02') ? $this->view('articles/newArticles', json_encode($result)) : $this->view('articles/articles', json_encode($result));
 	}
 
 	public function author($query = [], $author = DEFAULT_STRING) {

2e19a239749f624e31c961835cf9aeb54032c87c
suresh07 <soori_rozee@yahoo.com>
2018-08-20 13:12:44 +0530

    Text error fixed

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -10,18 +10,18 @@
                         <div class="col-md-5 mainArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=11" target="_blank">
-                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0011-0016.jpg">
+                                          <a href="<?=BASE_URL?>article/text/019/08/#page=11" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0011-0016.jpg">
                                                 <p class="title">ಧರ್ಮ - ಕನ್ನಡಿಗರ ಮನೋಧರ್ಮ</p>
                                                 <p class="content">ಸೂರ್ಯಸ್ತದ ವರ್ಣವೈಭವವೆಲ್ಲ ಕರಗಿ ನೀಲಮಿಶಿತ ಬೆಳಗಲ್ಲಿ ಲೀನವಾಗುತ್ತಿರುವ ಸಮಯ</p>
-                                                <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ">ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ</a></span>
+                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ">ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ</a></span>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=7" target="_blank">
-                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0007-0009.jpg">
+                                          <a href="<?=BASE_URL?>article/text/019/08/#page=7" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0007-0009.jpg">
                                                 <p class="title">ಭಗವಂತನ ಸ್ವರೂಪ</p>
                                                 <p class="content">ಆಧ್ಯಾತ್ಮಿಕ ಹಾದಿ ಒಂದು ರೀತಿಯಲ್ಲಿ ಕತ್ತಲಲ್ಲಿ ಪರದಾಡಿದಂತೆ. ಏನೂ ಕಾಣಿಸುತ್ತಿಲ್ಲ; ಏನೂ ಕಾಣದೆ</p>
                                           </a>
@@ -31,83 +31,83 @@
                         <div class="col-md-3 subArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=17" target="_blank">
-                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0017-0020.jpg">
+                                          <a href="<?=BASE_URL?>article/text/019/08/#page=17" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0017-0020.jpg">
                                                 <p class="title">ಆನಂದದ ಹುಡುಕಾಟ</p>
                                                 <p class="content">ಅತಿಶ್ರೇಷ್ಠ ಜೀವಿಯಾದ ಮಾನವನಿಂದ ಹಿಡಿದು ಅತಿಚಿಕ್ಕ ಸೂಕ್ಷ್ಮಜೀವಿಯ ವರೆಗಿನ ಪ್ರತಿಯೊಂದು</p>
-                                                <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ">ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ</a></span>
+                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ">ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ</a></span>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=28" target="_blank">
-                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0028-0032.jpg">
+                                          <a href="<?=BASE_URL?>article/text/019/08/#page=28" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0028-0032.jpg">
                                                 <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೦</p>
                                                 <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರ ವಯೋವೃದ್ಧ ಸಂನ್ಯಾಸಿ ಶಿಷ್ಯರಾದ ಸ್ವಾಮಿ ಅದ್ವೈತಾನಂದರು ಬೇಲೂರು ಮಠದಲ್ಲಿ...</p>
-                                                <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a></span>
+                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a></span>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=21" target="_blank">
-                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0021-0024.jpg">
+                                          <a href="<?=BASE_URL?>article/text/019/08/#page=21" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0021-0024.jpg">
                                                 <p class="title">ಶ್ರೀಮಾತೆ - ಭಾಗ ೧೯</p>
                                                 <p class="content">ಸುಪ್ರಸಿದ್ಧ ನಟಿಯರಾದ ತಿಂಕರಿ ಹಾಗೂ ತಾರಾ ಸುಂದರಿ ಇಬ್ಬರೂ ಕೆಲವೊಮ್ಮೆ ಶ್ರೀಮಾತೆಯವರಿಗೆ</p>
-                                                <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a></span>
+                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a></span>
                                           </a>
                                     </div>
                               </div>
                         </div>
                         <div class="col-md-4 cardArticle">
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=4" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0004-0004.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=4" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0004-0004.jpg">
                                           <p class="title">ಕೋರಿಕೆ</p>
                                           <p class="content">ಜಗವೆಲ್ಲ ನಗುತಿರಲಿ, ಜಗದಳುವು ನನಗಿರಲಿ, ನಾನಳಲು, ಜಗವೆನ್ನನೆತ್ತಿಕೊಳದೆ ನಾ ನಕ್ಕು</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=10" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0010-0010.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=10" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0010-0010.jpg">
                                           <p class="title">ದೇವರಿಗಾಗಿ ವ್ಯಾಕುಲತೆ</p>
                                           <p class="content">ಯಾವ ರೀತಿ ಮಗು ತನ್ನ ತಾಯಿಯನ್ನು ಕಾಣದೆ ದಿಗ್ಭ್ರಮೆಗೊಂಡು ಅದರ ಬಾಯಿಗೆ ಸಂದೇಶ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=25" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0025-0027.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=25" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0025-0027.jpg">
                                           <p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಅದ್ವೀತಿಯತೆ</p>
                                           <p class="content">ಜಗತ್ತಿನಲ್ಲಿ ಕಾಲಕಾಲಕ್ಕೆ ಮಹಾತ್ಮರು ಹುಟ್ಟಿ ಬಂದು ಧರ್ಮ ಸಂಸ್ಥಾಪನೆ ಮಾಡುತ್ತಾ ಬಂದಿದ್ದಾರೆ</p>
-                                          <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್">ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್</a></span>
+                                          <span><a class="author by" href="<?=BASE_URL?>articles/author/ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್">ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್</a></span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=33" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0033-0034.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=33" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0033-0034.jpg">
                                           <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು-೧೨</p>
                                           <p class="content">ಮಗೂ, ನಿನ್ನ ಪತ್ರವು ಸಿಕ್ಕಿ ಆನಂದಿತಳಾದೆ. ನೀನು ಬಿದ್ದು ನಿನ್ನ ಕಾಲು ಉಳುಕಿತ್ತು ಎಂದು ಈ ಮೊದಲೇ</p>
-                                          <span><a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ">ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ</a></span>
+                                          <span><a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ">ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ</a></span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=35" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0035-0037.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=35" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0035-0037.jpg">
                                           <p class="title">ಶ್ರೀವಲ್ಲಭಾಚಾರ್ಯರು-ಭಾಗ ೩</p>
                                           <p class="content">ಅಲಹಾಬಾದಿನಲ್ಲಿ ಇದ್ದ ನಿವೃತ್ತಿಯ ಜೀವಿತಾವಧಿಯಲ್ಲಿಯೇ ಅವರು ತಮ್ಮ ಬಹುತೇಕ ತಾತ್ತ್ವಿಕ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=38" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0038-0041.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=38" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0038-0041.jpg">
                                           <p class="title">ಕಬೀರದಾಸ</p>
                                           <p class="content">ಅದು ಸಂತಶ್ರೇಷ್ಠರಾದ ಸ್ವಾಮಿ ರಾಮಾನಂದರು ಜೀವಿಸಿದ್ದ ಕಾಲ. ಆಗ ಕಬೀರನೆಂಬ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/08/#page=42" target="_blank">
-                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/08/0042-0042.jpg">
+                                    <a href="<?=BASE_URL?>article/text/019/08/#page=42" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0042-0042.jpg">
                                           <p class="title">ಐರ್ಲೆಂಡ್ ರಾಮಕೃಷ್ಣ ವೇದಾಂತ...</p>
                                           <p class="content">ಐರ್ಲೆಂಡ್ ದೇಶದ ಆಡಳಿತ ಕೇಂದ್ರವಾದ ಡಬ್ಲಿನ್ನಲ್ಲಿ ೨೦೧೮ರ ಜೂನ್ ೧೭ ರಂದು ರಾಮಕೃಷ್ಣ ವೇದಾಂತ ಸೆಂಟರ್</p>
                                     </a>

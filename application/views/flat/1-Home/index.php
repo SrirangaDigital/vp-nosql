833f6cf51ef7f90b8bce59772a8c80aad3972a64
suresh07 <soori_rozee@yahoo.com>
2018-09-08 11:49:20 +0530

    September issue updated

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,7 +1,7 @@
 <div class="container dynamic-page">
       <div class="row justify-content-center">
             <div class="col-md-12 gap-above-med">
-                  <h1 class="text-left">ಪ್ರಸ್ತುತ ಸಂಚಿಕೆ (ಆಗಸ್ಟ್ - ೨೦೧೮)</h1>
+                  <h1>ಪ್ರಸ್ತುತ ಸಂಚಿಕೆ (ಸೆಪ್ಟೆಂಬರ್ - ೨೦೧೮)</h1>
             </div>
       </div>
       <div class="row justify-content-center">
@@ -10,20 +10,20 @@
                         <div class="col-md-5 mainArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/08/#page=11" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0011-0016.jpg">
-                                                <p class="title">ಧರ್ಮ - ಕನ್ನಡಿಗರ ಮನೋಧರ್ಮ</p>
-                                                <p class="content">ಸೂರ್ಯಸ್ತದ ವರ್ಣವೈಭವವೆಲ್ಲ ಕರಗಿ ನೀಲಮಿಶಿತ ಬೆಳಗಲ್ಲಿ ಲೀನವಾಗುತ್ತಿರುವ ಸಮಯ</p>
-                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ">ಕೆ. ಬಿ. ಪ್ರಭುಪ್ರಸದ್, ಪ್ರೋ</a></span>
+                                          <a href="<?=BASE_URL?>article/text/019/09/#page=21" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0021-0026.jpg" />
+                                                <p class="title">ಮಾತಾಪಿತರ ಮಹಾಲಯದಲ್ಲಿ</p>
+                                                <p class="content">ಒಡಲ ಭಾವ - ಮಡಿಲ ಭಾವಗಳು ಎಲ್ಲ ಜೀವಿಗಳಲ್ಲಿಯೂ ಸಮಾನುವಾಗಿದ್ದು ಸ್ವಂತ ಅನುಭವಕ್ಕೆ ಮಾತ್ರ  ಅಕ್ಕುವವಿಶಿಷ್ಟಅನುಭೂತಿ. ಆದರೆ ಭಾರತಿಯ್ಯರು....</p>
+                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ನಾರಾಯಣಿ ದಾಮೋದರ್">ನಾರಾಯಣಿ ದಾಮೋದರ್</a></span>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/08/#page=7" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0007-0009.jpg">
-                                                <p class="title">ಭಗವಂತನ ಸ್ವರೂಪ</p>
-                                                <p class="content">ಆಧ್ಯಾತ್ಮಿಕ ಹಾದಿ ಒಂದು ರೀತಿಯಲ್ಲಿ ಕತ್ತಲಲ್ಲಿ ಪರದಾಡಿದಂತೆ. ಏನೂ ಕಾಣಿಸುತ್ತಿಲ್ಲ; ಏನೂ ಕಾಣದೆ</p>
+                                          <a href="<?=BASE_URL?>article/text/019/09/#page=9" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0009-0011.jpg" />
+                                                <p class="title">ನಮ್ಮ ಅರಿವಿನ ಮಿತಿ</p>
+                                                <p class="content">ನಮ್ಮ ಬದುಕಿನಲ್ಲಿ ಅರಿವು ಎಂಬುದು ಬಹಳ ಮಹತ್ವದ ಪಾತ್ರ ವಹಿಸುತ್ತದೆ. ಶಿಕ್ಷಣ, ವೃತ್ತಿ, ಬೆಳವಣಿಗೆ, ಜ್ಞಾನದ ಆವಿಷ್ಕಾರ - ಎಲ್ಲವೂ ಒಂದಲ್ಲ ಒಂದು ರೀತಿ...</p>
                                           </a>
                                     </div>
                               </div>
@@ -31,85 +31,92 @@
                         <div class="col-md-3 subArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/08/#page=17" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0017-0020.jpg">
-                                                <p class="title">ಆನಂದದ ಹುಡುಕಾಟ</p>
-                                                <p class="content">ಅತಿಶ್ರೇಷ್ಠ ಜೀವಿಯಾದ ಮಾನವನಿಂದ ಹಿಡಿದು ಅತಿಚಿಕ್ಕ ಸೂಕ್ಷ್ಮಜೀವಿಯ ವರೆಗಿನ ಪ್ರತಿಯೊಂದು</p>
-                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ">ರಾಮಕೃಷ್ಣಾನಂದ, ಸ್ವಾಮಿ</a></span>
+                                          <a href="<?=BASE_URL?>article/text/019/09/#page=4" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0004-0004.jpg" />
+                                                <p class="title">ಶ್ರೀಕೃಷ್ಣಸ್ತುತಿಯಿಂದ</p>
+                                                <p class="content">ವಿಶ್ವದ ಸ್ವರೂಪನೂ, ವಿಶ್ವದ ಸ್ಥಿತಿ ಹಾಗೂ ಸಂಹಾರಕ್ಕೆ ಕಾರಣನೂ ಆದ, ಸಮಸ್ತ ವಿಶ್ವಕ್ಕೆ</p>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/08/#page=28" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0028-0032.jpg">
-                                                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೦</p>
-                                                <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರ ವಯೋವೃದ್ಧ ಸಂನ್ಯಾಸಿ ಶಿಷ್ಯರಾದ ಸ್ವಾಮಿ ಅದ್ವೈತಾನಂದರು ಬೇಲೂರು ಮಠದಲ್ಲಿ...</p>
-                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a></span>
+                                          <a href="<?=BASE_URL?>article/text/019/09/#page=7" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0007-0008.jpg" />
+                                                <p class="title">ಚಿಕಾಗೋ ಉಪನ್ಯಾಸಗಳು:೧೨೫ನೇ ವಾರ್ಷಿಕೋತ್ಸವ</p>
+                                                <p class="content">ಸೆಪ್ಟೆಂಬರ್ ೧೧ ಎಂದ ಕೊಡಲೇ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರು ೧೮೯೩ ರಲ್ಲಿ ಮಾಡಿದ ಚಿಕಾಗೋ</p>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/08/#page=21" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0021-0024.jpg">
-                                                <p class="title">ಶ್ರೀಮಾತೆ - ಭಾಗ ೧೯</p>
-                                                <p class="content">ಸುಪ್ರಸಿದ್ಧ ನಟಿಯರಾದ ತಿಂಕರಿ ಹಾಗೂ ತಾರಾ ಸುಂದರಿ ಇಬ್ಬರೂ ಕೆಲವೊಮ್ಮೆ ಶ್ರೀಮಾತೆಯವರಿಗೆ</p>
-                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a></span>
+                                          <a href="<?=BASE_URL?>article/text/019/09/#page=27" target="_blank">
+                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0027-0030.jpg" />
+                                                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು-೨೧</p>
+                                                <p class="content">ಗಿರೀಶ್ ಬಾಬು ಮತ್ತು ನಿರಂಜನ್ ಮಹಾರಾಜ ರೊಂದಿಗೆ ನಾನೊಮ್ಮೆ ಶ್ರೀಮಾತೆಯವರ ದರ್ಶನಕ್ಕೆ.</p>
+                                                <span>
+                                                      <a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
+                                                </span>
                                           </a>
                                     </div>
                               </div>
                         </div>
                         <div class="col-md-4 cardArticle">
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=4" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0004-0004.jpg">
-                                          <p class="title">ಕೋರಿಕೆ</p>
-                                          <p class="content">ಜಗವೆಲ್ಲ ನಗುತಿರಲಿ, ಜಗದಳುವು ನನಗಿರಲಿ, ನಾನಳಲು, ಜಗವೆನ್ನನೆತ್ತಿಕೊಳದೆ ನಾ ನಕ್ಕು</p>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=12" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0012-0012.jpg" />
+                                          <p class="title">ಸಾಧನೆ ಮತ್ತು ಶ್ರಮ</p>
+                                          <p class="content">ಒಂದು ಸ್ವಲ್ಪ ಸಾಧನೆ ಮಾಡಲೇಬೇಕು. ಸಾಧನೆ ಮಾಡುತ್ತ ಮಾಡುತ್ತ ಮುಂದುವರಿದರೆ, ಕ್ರಮೇಣ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=10" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0010-0010.jpg">
-                                          <p class="title">ದೇವರಿಗಾಗಿ ವ್ಯಾಕುಲತೆ</p>
-                                          <p class="content">ಯಾವ ರೀತಿ ಮಗು ತನ್ನ ತಾಯಿಯನ್ನು ಕಾಣದೆ ದಿಗ್ಭ್ರಮೆಗೊಂಡು ಅದರ ಬಾಯಿಗೆ ಸಂದೇಶ</p>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=13" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0013-0016.jpg" />
+                                          <p class="title">ಸತ್ಸಂಗದಿಂದ ನಿಸ್ಸಂಗ</p>
+                                          <p class="content">ಸತ್ಸಂಗತ್ವೇ ನಿಸ್ಸಂಗತ್ವಂ ಎಂಬುದು ಶ್ರೀಶಂಕರಾಚಾರ್ಯರ ಪ್ರಸಿದ್ಧವಾದ ಹೇಳಿಕೆ...</p>
+                                          <span>
+                                                <a class="author by" href="<?=BASE_URL?>articles/author/ನಿತ್ಯಸ್ಥಾನಂದ, ಸ್ವಾಮಿ">ನಿತ್ಯಸ್ಥಾನಂದ, ಸ್ವಾಮಿ</a>
+                                          </span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=25" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0025-0027.jpg">
-                                          <p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಅದ್ವೀತಿಯತೆ</p>
-                                          <p class="content">ಜಗತ್ತಿನಲ್ಲಿ ಕಾಲಕಾಲಕ್ಕೆ ಮಹಾತ್ಮರು ಹುಟ್ಟಿ ಬಂದು ಧರ್ಮ ಸಂಸ್ಥಾಪನೆ ಮಾಡುತ್ತಾ ಬಂದಿದ್ದಾರೆ</p>
-                                          <span><a class="author by" href="<?=BASE_URL?>articles/author/ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್">ಎಲ್. ಜಿ. ನಾಗೇಶ್ ರಾವ್</a></span>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=17" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0017-0020.jpg" />
+                                          <p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೨೦</p>
+                                          <p class="content">ದೈವೀ ಸಂಕಲ್ಪದಂತೆ ದುರದೃಷ್ಟವಶಾತ್ ಶ್ರೀಮಾತೆಯವರ ಒಬ್ಬ ಶಿಷ್ಯನು ಶಾಶ್ವತವಾಗಿ</p>
+                                          <span>
+                                                <a class="author by" href="<?=BASE_URL?>articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a>
+                                          </span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=33" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0033-0034.jpg">
-                                          <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು-೧೨</p>
-                                          <p class="content">ಮಗೂ, ನಿನ್ನ ಪತ್ರವು ಸಿಕ್ಕಿ ಆನಂದಿತಳಾದೆ. ನೀನು ಬಿದ್ದು ನಿನ್ನ ಕಾಲು ಉಳುಕಿತ್ತು ಎಂದು ಈ ಮೊದಲೇ</p>
-                                          <span><a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ">ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ</a></span>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=31" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0031-0032.jpg" />
+                                          <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು-೧೩</p>
+                                          <p class="content">ಮಗೂ, ಈಗ ತಾನೇ ನೀನು ಕಳುಹಿಸಿರುವ ೧೦ ರೂಪಾಯಿ ಮನಿಯಾರ್ಡರ್ ಸಿಕ್ಕಿತು</p>
+                                          <span>
+                                                <a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ">ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ</a>
+                                          </span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=35" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0035-0037.jpg">
-                                          <p class="title">ಶ್ರೀವಲ್ಲಭಾಚಾರ್ಯರು-ಭಾಗ ೩</p>
-                                          <p class="content">ಅಲಹಾಬಾದಿನಲ್ಲಿ ಇದ್ದ ನಿವೃತ್ತಿಯ ಜೀವಿತಾವಧಿಯಲ್ಲಿಯೇ ಅವರು ತಮ್ಮ ಬಹುತೇಕ ತಾತ್ತ್ವಿಕ</p>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=33" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0033-0037.jpg" />
+                                          <p class="title">ಉಪನಿಷತ್ತಿನ ಕಥೆಗಳು</p>
+                                          <p class="content">ದೇವದತ್ತನೆಂಬ ಬ್ರಾಹ್ಮಣನಿದ್ದನು ಅಂದಿನ ಕಾಲದ ವಾಡಿಕೆಯಂತೆ ವೇದಗಳ ಅಭ್ಯಾಸವನ್ನು ಮಾಡಿದ್ದನು</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=38" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0038-0041.jpg">
-                                          <p class="title">ಕಬೀರದಾಸ</p>
-                                          <p class="content">ಅದು ಸಂತಶ್ರೇಷ್ಠರಾದ ಸ್ವಾಮಿ ರಾಮಾನಂದರು ಜೀವಿಸಿದ್ದ ಕಾಲ. ಆಗ ಕಬೀರನೆಂಬ</p>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=38" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0038-0041.jpg" />
+                                          <p class="title">ಭಕ್ತ ರಾಖಾ</p>
+                                          <p class="content">ಹಿಂದೆ ಪಂಡರಾಪುರಲ್ಲಿ ಪಾಂಡುರಂಗನ ಭಕ್ತನೊಬ್ಬ ಇದ್ದ ಅವನ ಹೆಸರು ರಾಖಾ ಅವನ ಪತ್ನಿ ಪಾಖಾ ಅವರಿಗೆ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/08/#page=42" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/08/0042-0042.jpg">
-                                          <p class="title">ಐರ್ಲೆಂಡ್ ರಾಮಕೃಷ್ಣ ವೇದಾಂತ...</p>
-                                          <p class="content">ಐರ್ಲೆಂಡ್ ದೇಶದ ಆಡಳಿತ ಕೇಂದ್ರವಾದ ಡಬ್ಲಿನ್ನಲ್ಲಿ ೨೦೧೮ರ ಜೂನ್ ೧೭ ರಂದು ರಾಮಕೃಷ್ಣ ವೇದಾಂತ ಸೆಂಟರ್</p>
+                                    <a href="<?=BASE_URL?>article/text/019/09/#page=42" target="_blank">
+                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0042-0042.jpg" />
+                                          <p class="title">ಬ್ರಿಸ್ಬೇನಿನಲ್ಲಿ ವೇದಾಂತ ಸೆಂಟರ್ ಹಾಗೂ...</p>
+                                          <p class="content">ಆಸ್ಟ್ರೇಲಿಯಾದ ಸಿಡ್ನಿಯಲ್ಲಿರುವ ವೇದಾಂತ ಸೆಂಟರ್ನ ಬ್ರಿಸ್ಟೇನ್ ಶಾಖೆ ಹಾಗೂ ಅಲ್ಲಿನ ಬಹೂಪಯೋಗಿ ಕಟ್ಟಡದ</p>
                                     </a>
                               </div>
                         </div>

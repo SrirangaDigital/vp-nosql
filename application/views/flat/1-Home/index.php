1ae29bbc7b2362219fd51da56d3999845bd87c42
suresh07 <soori_rozee@yahoo.com>
2018-10-25 12:31:17 +0530

    October issue updated

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,7 +1,7 @@
 <div class="container dynamic-page">
       <div class="row justify-content-center">
             <div class="col-md-12 gap-above-med">
-                  <h1>ಪ್ರಸ್ತುತ ಸಂಚಿಕೆ (ಸೆಪ್ಟೆಂಬರ್ - ೨೦೧೮)</h1>
+                  <h1>ಪ್ರಸ್ತುತ ಸಂಚಿಕೆ (ಅಕ್ಟೋಬರ್ - ೨೦೧೮)</h1>
             </div>
       </div>
       <div class="row justify-content-center">
@@ -10,20 +10,19 @@
                         <div class="col-md-5 mainArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/09/#page=21" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0021-0026.jpg" />
-                                                <p class="title">ಮಾತಾಪಿತರ ಮಹಾಲಯದಲ್ಲಿ</p>
-                                                <p class="content">ಒಡಲ ಭಾವ - ಮಡಿಲ ಭಾವಗಳು ಎಲ್ಲ ಜೀವಿಗಳಲ್ಲಿಯೂ ಸಮಾನುವಾಗಿದ್ದು ಸ್ವಂತ ಅನುಭವಕ್ಕೆ ಮಾತ್ರ  ಅಕ್ಕುವವಿಶಿಷ್ಟಅನುಭೂತಿ. ಆದರೆ ಭಾರತಿಯ್ಯರು....</p>
-                                                <span><a class="author by" href="<?=BASE_URL?>articles/author/ನಾರಾಯಣಿ ದಾಮೋದರ್">ನಾರಾಯಣಿ ದಾಮೋದರ್</a></span>
+                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0031-0035.jpg">
+                                                <p class="title">ಆತ್ಮವನ್ನು ತಿಳಿದುಕೊಳ್ಳುವುದು ಹೇಗೆ?</p>
+                                                <p class="content">ಕೇನೋಪನಿಷತ್ತು ಸಾಮವೇದದ ಒಂದು ಅಂಗವಾಗಿದೆ. ಸಾಮವೇದದ ತಲವಕಾರ ಬ್ರಾಹ್ಮಣದಲ್ಲಿ ಈ ಉಪನಿಷತ್ತು ಬರುತ್ತದೆ</p>
                                           </a>
                                     </div>
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/09/#page=9" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0009-0011.jpg" />
-                                                <p class="title">ನಮ್ಮ ಅರಿವಿನ ಮಿತಿ</p>
-                                                <p class="content">ನಮ್ಮ ಬದುಕಿನಲ್ಲಿ ಅರಿವು ಎಂಬುದು ಬಹಳ ಮಹತ್ವದ ಪಾತ್ರ ವಹಿಸುತ್ತದೆ. ಶಿಕ್ಷಣ, ವೃತ್ತಿ, ಬೆಳವಣಿಗೆ, ಜ್ಞಾನದ ಆವಿಷ್ಕಾರ - ಎಲ್ಲವೂ ಒಂದಲ್ಲ ಒಂದು ರೀತಿ...</p>
+                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0007-0010.jpg">
+                                                <p class="title">ವೇದ, ವೇದಾಂತಗಳ ಮಹತ್ವ</p>
+                                                <p class="content">ನಮ್ಮ ದೇಶದಲ್ಲಿ ಬೆಳೆದು ಬಂದ ಧಾರ್ಮಿಕ, ಸಾಂಸ್ಕೃತಿಕ ಪರಂಪರಯಲ್ಲಿ ವೇದ ಹಾಗೂ ವೇದಾಂತಗಳಿಗೆ ಅತಿಹೆಚ್ಚಿನ ಪ್ರಾಧಾನ್ಯವಿದೆ</p>
                                           </a>
                                     </div>
                               </div>
@@ -31,30 +30,36 @@
                         <div class="col-md-3 subArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/09/#page=4" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0004-0004.jpg" />
-                                                <p class="title">ಶ್ರೀಕೃಷ್ಣಸ್ತುತಿಯಿಂದ</p>
-                                                <p class="content">ವಿಶ್ವದ ಸ್ವರೂಪನೂ, ವಿಶ್ವದ ಸ್ಥಿತಿ ಹಾಗೂ ಸಂಹಾರಕ್ಕೆ ಕಾರಣನೂ ಆದ, ಸಮಸ್ತ ವಿಶ್ವಕ್ಕೆ</p>
-                                          </a>
-                                    </div>
-                              </div>
-                              <div class="row">
-                                    <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/09/#page=7" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0007-0008.jpg" />
-                                                <p class="title">ಚಿಕಾಗೋ ಉಪನ್ಯಾಸಗಳು:೧೨೫ನೇ ವಾರ್ಷಿಕೋತ್ಸವ</p>
-                                                <p class="content">ಸೆಪ್ಟೆಂಬರ್ ೧೧ ಎಂದ ಕೊಡಲೇ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರು ೧೮೯೩ ರಲ್ಲಿ ಮಾಡಿದ ಚಿಕಾಗೋ</p>
-                                          </a>
-                                    </div>
-                              </div>
-                              <div class="row">
-                                    <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/09/#page=27" target="_blank">
-                                                <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0027-0030.jpg" />
-                                                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು-೨೧</p>
-                                                <p class="content">ಗಿರೀಶ್ ಬಾಬು ಮತ್ತು ನಿರಂಜನ್ ಮಹಾರಾಜ ರೊಂದಿಗೆ ನಾನೊಮ್ಮೆ ಶ್ರೀಮಾತೆಯವರ ದರ್ಶನಕ್ಕೆ.</p>
+                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0020-0023.jpg">
+                                                <p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೨೧</p>
+                                                <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣ ಕಥಾಮೃತದ ಯಾವುದೇ ಸಂಪುಟ ಪ್ರಕಟವಾಗಿ ಹೊರಬಂದರೂ ಮಾಸ್ಟರ್ ಮಹಾಶಯ ಶ್ರೀಮಾತೆ</p>
                                                 <span>
-                                                      <a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
+                                                      <a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a>
+                                                </span>
+                                          </a>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div col-md-12>
+                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0024-0027.jpg">
+                                                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೨</p>
+                                                <p class="content">೧೯೧೪ನೇ ವರ್ಷ ಬಂದೇ ಬಿಟ್ಟಿತು. ಅಂದುಕೊಂಡಿದ್ದಂತೆ ವಿರಜಾನಂದರಿಗೆ ಜನವರಿ ೧ ರಿಂದ ಸ್ವಾಮೀಜಿಯವರ ಪುಸ್ತಕಕ್ಕೆ ಕೈಹಾಕಲಾಗಲಿಲ್ಲ</p>
+                                                <span>
+                                                      <a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
+                                                </span>
+                                          </a>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div col-md-12>
+                                          <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                                <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0028-0030.jpg">
+                                                <p class="title">ಸ್ವಾಮಿ ಭಾಸ್ಕರಾನಂದ</p>
+                                                <p class="content">ಸ್ವಾಮಿ ಭಾಸ್ಕರಾನಂದ ಸರಸ್ವತಿಯರು ೧೯ನೆಯ ಶತಮಾನದಲ್ಲಿ ಕಾಶಿಯಲ್ಲಿದ್ದ ಪ್ರಸಿದ್ಧ ಸಂತರು. ಅವರು ೧೮೩೩ ರಿಂದ ೧೮೯೯ರ ವರೆಗೆ</p>
+                                                <span>
+                                                      <a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಅಕ್ಷಯ ಕುಮಾರ ಬ್ಯಾನರ್ಜಿ, ಸ್ವಾಮಿ">ಅಕ್ಷಯ ಕುಮಾರ ಬ್ಯಾನರ್ಜಿ, ಸ್ವಾಮಿ</a>
                                                 </span>
                                           </a>
                                     </div>
@@ -62,61 +67,51 @@
                         </div>
                         <div class="col-md-4 cardArticle">
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=12" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0012-0012.jpg" />
-                                          <p class="title">ಸಾಧನೆ ಮತ್ತು ಶ್ರಮ</p>
-                                          <p class="content">ಒಂದು ಸ್ವಲ್ಪ ಸಾಧನೆ ಮಾಡಲೇಬೇಕು. ಸಾಧನೆ ಮಾಡುತ್ತ ಮಾಡುತ್ತ ಮುಂದುವರಿದರೆ, ಕ್ರಮೇಣ</p>
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0004-0004.jpg">
+                                          <p class="title">ಶ್ರೀ ಲಕ್ಷ್ಮೀ ಸ್ತುತಿ</p>
+                                          <p class="content">ಹಿರಣ್ಮಯಿಯಾದ ಲಕ್ಷ್ಮೀಯನ್ನೇ ನಾನು ಸದಾ ಭಜಿಸುತ್ತೇನೆ, ಹೀನ ಮಾನವನ ಆಶ್ರಯವನ್ನು ತ್ಯಜಿಸುತ್ತೇನೆ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=13" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0013-0016.jpg" />
-                                          <p class="title">ಸತ್ಸಂಗದಿಂದ ನಿಸ್ಸಂಗ</p>
-                                          <p class="content">ಸತ್ಸಂಗತ್ವೇ ನಿಸ್ಸಂಗತ್ವಂ ಎಂಬುದು ಶ್ರೀಶಂಕರಾಚಾರ್ಯರ ಪ್ರಸಿದ್ಧವಾದ ಹೇಳಿಕೆ...</p>
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0011-0011.jpg">
+                                          <p class="title">ಬ್ರಹ್ಮಸತ್ಯಂ ಜಗನ್ಮಿಥ್ಯಾ</p>
+                                          <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರು: ಹೌದು, ಭಗವಂತನ ಸಾಕ್ಷಾತ್ಕಾರವನ್ನು ಪಡೆಯದವರೆಗೆ ಈ ಜಗತ್ತು ವಿಥ್ಯೆಯಾದುದೇನು</p>
+                                    </a>
+                              </div>
+                              <div class="cards-wrapper">
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0012-0015.jpg">
+                                          <p class="title">ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರ ಯುವ ದಿನಗಳು</p>
+                                          <p class="content">ನಾನು ಮೊದಲು ವಿವೇಕಾನಂದರನ್ನು ಭೇಟಿಯಾದದ್ದು ೧೮೮೧ರಲ್ಲಿ ಜನರಲ್..</p>
                                           <span>
-                                                <a class="author by" href="<?=BASE_URL?>articles/author/ನಿತ್ಯಸ್ಥಾನಂದ, ಸ್ವಾಮಿ">ನಿತ್ಯಸ್ಥಾನಂದ, ಸ್ವಾಮಿ</a>
+                                                <a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಬ್ರಜೀಂದ್ರನಾಥ ಸೀಲ್">ಬ್ರಜೀಂದ್ರನಾಥ ಸೀಲ್</a>
                                           </span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=17" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0017-0020.jpg" />
-                                          <p class="title">ಶ್ರೀಮಾತೆ-ಭಾಗ ೨೦</p>
-                                          <p class="content">ದೈವೀ ಸಂಕಲ್ಪದಂತೆ ದುರದೃಷ್ಟವಶಾತ್ ಶ್ರೀಮಾತೆಯವರ ಒಬ್ಬ ಶಿಷ್ಯನು ಶಾಶ್ವತವಾಗಿ</p>
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0016-0019.jpg">
+                                          <p class="title">ಜೀವನದ ಮೌಲ್ಯಗಳ ಮೂಲ:ಸನಾತನ...</p>
+                                          <p class="content">ಸನಾತನ ಧರ್ಮವು ವಿಶ್ವಸಮ್ಮತ ವಿಚಾರವನ್ನು ತತ್ತ್ವ ಶಾಸ್ತ್ರ ಸಂಬಂಧಿತ ಅಭಿಪ್ರಾಯಗಳ ಆಧಾರಗಳ..</p>
                                           <span>
-                                                <a class="author by" href="<?=BASE_URL?>articles/author/ಆಶುತೋಷ್ ಮಿತ್ರ">ಆಶುತೋಷ್ ಮಿತ್ರ</a>
+                                                <a class="author by" href="http://192.168.1.101/vp-nosql/articles/author/ಸುಖಾತ್ಮಾನಂದ, ಸ್ವಾಮಿ">ಸುಖಾತ್ಮಾನಂದ, ಸ್ವಾಮಿ</a>
                                           </span>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=31" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0031-0032.jpg" />
-                                          <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು-೧೩</p>
-                                          <p class="content">ಮಗೂ, ಈಗ ತಾನೇ ನೀನು ಕಳುಹಿಸಿರುವ ೧೦ ರೂಪಾಯಿ ಮನಿಯಾರ್ಡರ್ ಸಿಕ್ಕಿತು</p>
-                                          <span>
-                                                <a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ">ಪ್ರವ್ರಾಜಿಕಾ ಧರ್ಮಪ್ರಾಣಾ</a>
-                                          </span>
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0036-0039.jpg">
+                                          <p class="title">ಶ್ರೀಜಯದೇವ -೧</p>
+                                          <p class="content">ಹನ್ನೆರಡನೇ ಶತಮಾನದಲ್ಲಿ ಪುರೀಕ್ಷೇತ್ರಕ್ಕೆ ಸಮೀಪದ ಕೆಂದಬಿಲ್ವ ಎಂಬ ಒಂದು ಗ್ರಾಮದಲ್ಲಿ ಭೋಜದೇವ ಹಾಗೂ ಭಾಮಾದೇವಿ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=33" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0033-0037.jpg" />
-                                          <p class="title">ಉಪನಿಷತ್ತಿನ ಕಥೆಗಳು</p>
-                                          <p class="content">ದೇವದತ್ತನೆಂಬ ಬ್ರಾಹ್ಮಣನಿದ್ದನು ಅಂದಿನ ಕಾಲದ ವಾಡಿಕೆಯಂತೆ ವೇದಗಳ ಅಭ್ಯಾಸವನ್ನು ಮಾಡಿದ್ದನು</p>
-                                    </a>
-                              </div>
-                              <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=38" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0038-0041.jpg" />
-                                          <p class="title">ಭಕ್ತ ರಾಖಾ</p>
-                                          <p class="content">ಹಿಂದೆ ಪಂಡರಾಪುರಲ್ಲಿ ಪಾಂಡುರಂಗನ ಭಕ್ತನೊಬ್ಬ ಇದ್ದ ಅವನ ಹೆಸರು ರಾಖಾ ಅವನ ಪತ್ನಿ ಪಾಖಾ ಅವರಿಗೆ</p>
-                                    </a>
-                              </div>
-                              <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/09/#page=42" target="_blank">
-                                          <img class="img-fluid" src="<?=BASE_URL?>public/data/019/09/0042-0042.jpg" />
-                                          <p class="title">ಬ್ರಿಸ್ಬೇನಿನಲ್ಲಿ ವೇದಾಂತ ಸೆಂಟರ್ ಹಾಗೂ...</p>
-                                          <p class="content">ಆಸ್ಟ್ರೇಲಿಯಾದ ಸಿಡ್ನಿಯಲ್ಲಿರುವ ವೇದಾಂತ ಸೆಂಟರ್ನ ಬ್ರಿಸ್ಟೇನ್ ಶಾಖೆ ಹಾಗೂ ಅಲ್ಲಿನ ಬಹೂಪಯೋಗಿ ಕಟ್ಟಡದ</p>
+                                    <a href="http://192.168.1.101/vp-nosql/article/text/019/10/#page=1" target="_blank">
+                                          <img class="img-fluid" src="http://192.168.1.101/vp-nosql/public/data/019/10/0040-0042.jpg">
+                                          <p class="title">ನೆರೆ ಪರಿಹಾರ ಕಾರ್ಯ</p>
+                                          <p class="content">ಪೊನ್ನಂಪೇಟೆಯ ಶ್ರೀರಾಮಕೃಷ್ಣ ಶಾರದಶ್ರಮವು ಕೊಡಗಿನ ಅನೇಕ ಕಡೆಗಳಲ್ಲಿ ನೆರೆ ಪರಿಹಾರ ವಿತರಣೆಯ ಸೇವೆಯನ್ನು ಕೈಗೊಂಡಿದೆ. ಮೊದಲ</p>
                                     </a>
                               </div>
                         </div>

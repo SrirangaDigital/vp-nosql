6448322f580f3617071eadf378ee164cea89b4ef
Suresh <soori_rozee@yahoo.com>
2019-01-11 15:20:52 +0530

    Relative page number updation

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -10,7 +10,7 @@
                         <div class="col-md-5 mainArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                          <a href="<?=BASE_URL?>article/text/019/12/#page=7" target="_blank">
                                                 <img class="img-fluid" src="<?=DATA_URL?>019/12/0007-0009.jpg">
                                                 <p class="title">ಸಾಧನೆಯ ಬಗೆಗಳು</p>
                                                 <p class="content">ಪ್ರಪಂಚದಲ್ಲಿ ಸಸ್ಯ, ಪ್ರಾಣಿ-ಪಕ್ಷಿ, ಕ್ರಿಮಿಕೀಟಗಳು ಹಾಗೂ ಮಾನವ-ಎಲ್ಲರೂ ಜೀವಿಸುವುದು ಬೇರೆ ಬೇರೆ ರೀತಿಯಲ್ಲಿ</p>
@@ -19,7 +19,7 @@
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                          <a href="<?=BASE_URL?>article/text/019/12/#page=26" target="_blank">
                                                 <img class="img-fluid" src="<?=DATA_URL?>019/12/0026-0031.jpg">
                                                 <p class="title">ಆನಂದದ ಹುಡುಕಾಟ-ಭಾಗ ೧</p>
                                                 <p class="content">ಭಗವಂತನ ಮೂಲ ಸ್ವರೂಪವೇ ಆಗಿರುವ ಆನಂದದ ಸುತ್ತ ಎಲ್ಲ ಜೀವಿಗಳ ಜಗತ್ತಿನ ಜೀವನ ಅವಲಂಬಿತವಾಗಿದೆ. ಪ್ರತಿಯೊಂದು ಜೀವಿಯೂ</p>
@@ -33,7 +33,7 @@
                         <div class="col-md-3 subArticle">
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                          <a href="<?=BASE_URL?>article/text/019/12/#page=11" target="_blank">
                                                 <img class="img-fluid" src="<?=DATA_URL?>019/12/0011-0014.jpg">
                                                 <p class="title">ಕರುಣಾಮಯಿ</p>
                                                 <p class="content">ನಿರಾಕಾರಾಪಿ ಸಾಕಾರಾ ಕಸ್ತ್ವಂ ವೇದಿತುಮರ್ಹತಿ ಎಂದು ಶಾಸ್ತ್ರ ಹೇಳುತ್ತದೆ ನಿರಾಕಾರವಾದರೂ ಆಕಾರವನ್ನು ಹೊಂದುವುದು ಯಾಕೆ</p>
@@ -45,7 +45,7 @@
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                          <a href="<?=BASE_URL?>article/text/019/12/#page=15" target="_blank">
                                                 <img class="img-fluid" src="<?=DATA_URL?>019/12/0015-0019.jpg">
                                                 <p class="title">ನಮ್ಮ ಸಮಸ್ಯೆಗಳು ಮತ್ತು ವಿವೇಕಾನಂದರ ಮಾನವೀಯ ಪರಿಹಾರ</p>
                                                 <p class="content">ಜಗತ್ತು ಆಧುನಿಕವಾದಂತೆಲ್ಲ ನಮ್ಮ ಬದುಕಿನ ಸಮಸ್ಯೆಗಳೂ ಅಧಿಕವಾಗುತ್ತಿವೆ, ಸಂಕೀರ್ಣವಾಗುತ್ತಿವೆ. ಹಲವು ಸಮಸ್ಯೆಗಳಿಗೆ ಪರಿಹಾರವೇ ಇಲ್ಲವೇನೋ ಅನ್ನಿಸುತ್ತವೆ</p>
@@ -57,7 +57,7 @@
                               </div>
                               <div class="row">
                                     <div col-md-12>
-                                          <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                          <a href="<?=BASE_URL?>article/text/019/12/#page=24" target="_blank">
                                                 <img class="img-fluid" src="<?=DATA_URL?>019/12/0024-0025.jpg">
                                                 <p class="title">ಶ್ರೀ ಶ್ರೀರಾಮಕೃಷ್ಣ ಭಾಗವತಮ್</p>
                                                 <p class="content">ಅನೆಯನ್ನು ಕಾಣಲು ಹೋದ ಕುರುಡುರು ಅದರ ಒಂದೊಂದು ಅವಯವವನ್ನು ಮುಟ್ಟಿ, ಅದು ಗೋಡೆಯಂತೆ ಇದೆ, ಮೊರದಂತೆ ಇದೆ</p>
@@ -67,21 +67,21 @@
                         </div>
                         <div class="col-md-4 cardArticle">
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=4" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0004-0004.jpg">
                                           <p class="title">ಶ್ರೀಶಾರದಾದೇವ್ಯಷ್ಟಕದಿಂದ</p>
                                           <p class="content">ಬ್ರಹ್ಮಸ್ವರೂಪಿಣಿ, ಶಿವೆ, ಶುಭವನ್ನು ನೀಡುವವಳೆ, ಶರಣ್ಯಳೆ, ಚೈತನ್ಯವನ್ನು ನೀಡುವವಳೆ, ಸಂಸಾರಸಾಗರದ ದಡವನ್ನು</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=10" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0010-0010.jpg">
                                           <p class="title">ಸಾಧಕನ ಭಾವಗಳು</p>
                                           <p class="content">ಆತನಿಗೆ ಹಲವಾರು ವಿಧದಿಂದ ಸೇವೆ ಸಲ್ಲಿಸಬಹುದು. ಪ್ರೇಮಕ ಭಕ್ತ ಆತನೊಡನೆ ವಿವಿಧ ರೂಪದಲ್ಲಿ ಆನಂದಪಡುತ್ತಾನೆ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=20" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0020-0023.jpg">
                                           <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು-೨೪</p>
                                           <p class="content">ಮಠಾಧ್ಯಕ್ಷರಾದ ಸ್ವಾಮಿ ಬ್ರಹ್ಮಾನಂದರೊಂದಿಗೆ ಎಲ್ಲರೂ ಮಿಸೆಸ್ ಸೇವಿಯರ್ ಅವರನ್ನು ಆದರದಿಂದ ಸ್ವಾಗತಿಸಿದರು ವಿರಜಾನಂದರಿಗೂ</p>
@@ -91,7 +91,7 @@
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=32" target="_blank">
                                           <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು-೧೫</p>
                                           <p class="content">ತಾಯಿ, ನಿನ್ನ ಪತ್ರವು ಸಿಕ್ಕಿ ಸಮಾಧಾನವಾಯಿತು. ನಿನಗೆ ನನ್ನ ಆಶೀರ್ವಾದ ಹಾಗೂ ಶೋಕಹರಣನಿಗೆ ಮತ್ತು ಸುಮತಿಗೆ ನನ್ನ ಆಶೀರ್ವಾದ ತಿಳಿಸು</p>
                                           <span>
@@ -100,21 +100,21 @@
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=33" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0033-0037.jpg">
                                           <p class="title">ಪರಾ ವಿದ್ಯೆ ಯಾವುದು? ಅಪರಾ ವಿದ್ಯೆ...</p>
                                           <p class="content">ಮುಂಡಕೋಪನಿಷತ್ತು ಅಥರ್ವಣವೇದಕ್ಕೆ ಸೇರಿದ್ದು. ಈ ಉಪನಿಷತ್ತು ಆಕಾರದಲ್ಲಿ ಸಣ್ಣದು. ಬ್ರಹ್ಮವಿದ್ಯೆಯೇ ಇದರ ಮುಖ್ಯ ವಿಷಯ</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=38" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0038-0041.jpg">
                                           <p class="title">ಶಿವಾಜಿಯ ಮೇಲೆ ವಿಠಲನ ಕೃಪೆ</p>
                                           <p class="content">ಪುಣೆಯ ವಿಠೋಬಾ ದೇವಸ್ಥಾನದಲ್ಲಿ ಹೌದು, ಅವನೊಬ್ಬ ದೊಡ್ಡ ಭಕ್ತ ಭಗವಾನ್ ಪಾಂಡುರಂಗನೊಂದಿಗೆ ಅವನು ನೇರ ಮಾತುಕತೆ...</p>
                                     </a>
                               </div>
                               <div class="cards-wrapper">
-                                    <a href="<?=BASE_URL?>article/text/019/12/#page=1" target="_blank">
+                                    <a href="<?=BASE_URL?>article/text/019/12/#page=42" target="_blank">
                                           <img class="img-fluid" src="<?=DATA_URL?>019/12/0042-0042.jpg">
                                           <p class="title">ರಾಮಕೃಷ್ಣ ಮಹಾಸಂಘದ ಅಕ್ಟೋಬರ್..</p>
                                           <p class="content">ಅಕ್ಟೋಬರ್ ೧೬ರಿಂದ ೧೯ರ ವರೆಗೆ ಬೇಲೂರು ಮಠ ಹಾಗೂ ಮಹಾಸಂಘದ ವಿವಿಧ ಕೇಂದ್ರಗಳಲ್ಲಿ ದರ್ಗಾಪೂಜೆಯನ್ನು ವಿವಿಧ ಸಡಗರ...</p>

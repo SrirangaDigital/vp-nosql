4c3ddd41e77660c14842e12a5a2dba90de2621af
Suresh <soori_rozee@yahoo.com>
2019-04-02 17:16:20 +0530

    url changed

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -10,20 +10,20 @@
         <div class="col-md-5 mainArticle">
           <div class="row">
             <div col-md-12>
-              <a href="http://localhost/vp-nosql/article/text/020/03/#page=24" target="_blank">
-                <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0024-0028.jpg" />
+              <a href="<?=BASE_URL?>article/text/020/03/#page=24" target="_blank">
+                <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0024-0028.jpg" />
                 <p class="title">ಆನಂದದ ಹುಡುಕಾಟ - ೩</p>
                 <p class="content">ಭಗವಾನ್ ಬುದ್ಧನ ಉಪದೇಶವು ದುಃಖದ ಸತ್ಯತೆ ಮತ್ತು ಅದರಿಂದ ಪಾರಾಗುವ ಉಪಾಯವನ್ನು ಆಧರಿಸಿದೆ. ಅವನ ಪ್ರಕಾರ ರೋಗ, ಜರೆ, ಮೃತ್ಯು – ಇವೆಲ್ಲವೂ ....</p>
                 <span>
-                  <a class="author by" href="http://localhost/vp-nosql/articles/author/ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ">ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ">ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
               </a>
             </div>
           </div>
           <div class="row">
             <div col-md-12>
-              <a href="http://localhost/vp-nosql/article/text/020/03/#page=7" target="_blank">
-                <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0007-0009.jpg" />
+              <a href="<?=BASE_URL?>article/text/020/03/#page=7" target="_blank">
+                <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0007-0009.jpg" />
                 <p class="title">ಸಂನ್ಯಾಸ ಮತ್ತು ಗೃಹಸ್ಥ ಜೀವನ</p>
                 <p class="content">ಪ್ರಪಂಚದ ಪ್ರಮುಖ ಧರ್ಮಗಳೆಲ ಸಂನ್ಯಾಸ ಜೀವನಕೆ ವವರು ಅಸಾಧಾರಣ ಮನೋಬಲ, ವೈರಾಗ್ಯ ಹಾಗೂ ನಿಶ್ಚಿತ ಮಹತ್ವ ಕೊಟ್ಟಿರುವುದನ್ನು ನಾವು ...</p>
               </a>
@@ -33,27 +33,27 @@
         <div class="col-md-3 subArticle">
           <div class="row">
             <div col-md-12>
-              <a href="http://localhost/vp-nosql/article/text/020/03/#page=11" target="_blank">
-                <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0011-0014.jpg" />
+              <a href="<?=BASE_URL?>article/text/020/03/#page=11" target="_blank">
+                <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0011-0014.jpg" />
                 <p class="title">ಶ್ರೀಮಾತೆಯವರೊಂದಿಗೆ - ಭಾಗ ೧</p>
                 <p class="content">ಶ್ರೀಮಾತೆಯವರ ಶಿಷ್ಯರೂ, ಅವರ ಆತ್ಮೀಯ ಸೇವಕರೂ ಆಗಿದ್ದ ಲೇಖಕರು ರಾಮಕೃಷ್ಣ ...</p>
                 <span>
-                  <a class="author by" href="http://localhost/vp-nosql/articles/author/ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ">ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ">ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
                 <span>
-                  <a class="author by" href="http://localhost/vp-nosql/articles/author/ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ">ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ">ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
               </a>
             </div>
           </div>
           <div class="row">
             <div col-md-12>
-              <a href="http://localhost/vp-nosql/article/text/020/03/#page=15" target="_blank">
-                <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0015-0017.jpg" />
+              <a href="<?=BASE_URL?>article/text/020/03/#page=15" target="_blank">
+                <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0015-0017.jpg" />
                 <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೬</p>
                 <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಸಂನ್ಯಾಸಿ ಶಿಷ್ಯರಲ್ಲೊಬ್ಬರಾದ ಸ್ವಾಮಿ ಅದ್ದುತಾ ಕೊಂಡರು ನಂದರು ೧೯೨೦ರ ಏಪ್ರಿಲ್ ...</p>
                 <span>
-                  <a class="author by" href="http://localhost/vp-nosql/articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
               </a>
             </div>
@@ -61,63 +61,63 @@
         </div>
         <div class="col-md-4 cardArticle">
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=4" target="_blank">
-              <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0004-0006.jpg" />
+            <a href="<?=BASE_URL?>article/text/020/03/#page=4" target="_blank">
+              <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0004-0006.jpg" />
               <p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಭಜನೆ</p>
               <p class="content">ಅರೂಪ ಸಾಯರೆ ಲೀಲಾ ಲಹರಿ ಉಠಿಲೊ ಮೃದುಲ ಕರುಣಾಬಾಯ್ ಆದಿ ಅಂತಹೀನ ಅಖಂಡೇ ವಿಲೀನ, ಮಾಯಾಯ್ ಥ್ರಿಲೇ...</p>
               <span>
-                <a class="author by" href="http://localhost/vp-nosql/articles/author/ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ">ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ">ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=10" target="_blank">
-              <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0010-0010.jpg" />
+            <a href="<?=BASE_URL?>article/text/020/03/#page=10" target="_blank">
+              <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0010-0010.jpg" />
               <p class="title">ಆಧ್ಯಾತ್ಮಿಕ ಜೀವನದ ಸಾರ</p>
               <p class="content">ನಿಮ್ಮ ಚೀಲ' ಎಂಬುದಾಗಿ ಆ ನೀರು ಹೊರುವ ತಿನ್ನುವುದು, ತಂಬಾಕು ಸೇವಿಸುವುದು, ಮೈಗೆ ವ್ಯಕ್ತಿ ಹೇಳಿದುದರ ಅರ್ಥ ದೇಹ, ಹೊಟ್ಟೆ ಇತ್ಯಾದಿ....</p>
               <span>
-                <a class="author by" href="http://localhost/vp-nosql/articles/author/ವಚನವೇದದಿಂದ">ವಚನವೇದದಿಂದ</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ವಚನವೇದದಿಂದ">ವಚನವೇದದಿಂದ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=29" target="_blank">
-              <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0029-0032.jpg" />
+            <a href="<?=BASE_URL?>article/text/020/03/#page=29" target="_blank">
+              <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0029-0032.jpg" />
               <p class="title">ಶರತ್‌ಚಂದ್ರ ಚಕ್ರವರ್ತಿ - ಭಾಗ ೧</p>
               <p class="content">ದಿವ್ಯತೆಯ ಸರ್ಶದಿಂದ ಅತ್ಯಂತ ಸಾಮಾನ್ಯ ವ್ಯಕ್ತಿಯೂ ಅತಿಶ್ರೇಷ್ಠ ಪದವಿಯನ್ನು ಹೊಂದುವನು ಎನ್ನುವ ಮಾತಿಗೆ ಉತ್ತಮ ನಿದರ್ಶನವೇ ...</p>
               <span>
-                <a class="author by" href="http://localhost/vp-nosql/articles/author/ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ">ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ">ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=38" target="_blank">
-              <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0038-0041.jpg" />
+            <a href="<?=BASE_URL?>article/text/020/03/#page=38" target="_blank">
+              <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0038-0041.jpg" />
               <p class="title">ಕೃಪೆ ತಂದ ವಿಜಯ</p>
               <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರು ಆಗ ದಕ್ಷಿಣೇಶ್ವರದಲ್ಲಿ ವಾಸವಾಗಿದ್ದರು. ಒಂದು ದಿನ ಮಧುರ ಬಾಬುವಿನ ಅರಮನೆಯಲ್ಲಿ ಸ್ವಾಮಿ, ನಮ್ಮ ದೇವಾಲಯದ...</p>
               <span>
-                <a class="author by" href="http://localhost/vp-nosql/articles/author/ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ">ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ">ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=42" target="_blank">
-              <img class="img-fluid" src="http://localhost/vp-nosql/public/data/020/03/0042-0042.jpg" />
+            <a href="<?=BASE_URL?>article/text/020/03/#page=42" target="_blank">
+              <img class="img-fluid" src="<?=PHY_DATA_URL?>020/03/0042-0042.jpg" />
               <p class="title">ಮೈಸೂರು ಆಶ್ರಮದ ಕಾರ್ಯಕ್ರಮಗಳು</p>
               <p class="content">'ವಿವೇಕ ಶಿಕ್ಷಣ'ದ ವಾರ್ಷಿಕೋತ್ಸವ : ಮೈಸೂರು ಶ್ರೀರಾಮಕೃಷ್ಣ ಆಶ್ರಮವು ಕಳೆದ ೧೯ ವರ್ಷಗಳಿಂದ ನಡೆಸಿಕೊಂಡು ಬರುತ್ತಿರುವ, ಆರ್ಥಿಕ ಹಾಗೂ...</p>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=18" target="_blank">
+            <a href="<?=BASE_URL?>article/text/020/03/#page=18" target="_blank">
               <p class="title">ಅಪರೂಪದ ಗುರು ಅಲ್ಲಮಪ್ರಭು</p>
               <p class="content">ಹನ್ನೆರಡನೇ ಶತಮಾನದಲ್ಲಿ ಶರಣ ಚಳವಳಿಯ ನೇತಾರರಲ್ಲಿ ದಿವ್ಯ ಜ್ಯೋತಿಯಾಗಿ ಬೆಳಗಿದ ಶ್ರೇಷ್ಠ ಸಂತರೇ ಅಲ್ಲಮ ಪ್ರಭು ದೇವರು. ಬಸವೇಶ್ವರರೇ ಮೊದಲಾದ ಮಹಾನ್ ಶರಣರ ಒಡನಾಡಿಯೂ...</p>
               <span>
-                <a class="author by" href="http://localhost/vp-nosql/articles/author/ಶಶಿಕಲಾ ಶ್ರೀಧರ್">ಶಶಿಕಲಾ ಶ್ರೀಧರ್</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಶಶಿಕಲಾ ಶ್ರೀಧರ್">ಶಶಿಕಲಾ ಶ್ರೀಧರ್</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="http://localhost/vp-nosql/article/text/020/03/#page=33" target="_blank">
+            <a href="<?=BASE_URL?>article/text/020/03/#page=33" target="_blank">
               <p class="title">ಸಾವಿನಾಚೆಗೆ, ಚೈತನ್ಯದ ಬೆಳಕಿಗೆ</p>
               <p class="content">ಸ್ವಾಮಿ ವೀರೇಶಾನಂದ (ಈ ನಾಟಕವನ್ನು ಕಠೋಪನಿಷತ್ತಿನ ಆಧಾರದ ಮೇಲೆ ರಚಿಸಲಾಗಿದೆ. ಕಠೋಪನಿಷತ್ತು ಉಪನಿಷತ್ತುಗಳಲ್ಲೆಲ್ಲ ಪ್ರಸಿದ್ಧವಾದುದು. ಇಲ್ಲಿ ಬಾಲಕ ನಚಿಕೇತನಿಗೆ ಮೃತ್ಯುದೇವತೆಯಾದ...</p>
             </a>

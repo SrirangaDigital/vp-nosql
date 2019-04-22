8b72f5eed288acc1995372bbc20afc19ae115ce4
Suresh <soori_rozee@yahoo.com>
2019-04-22 18:47:54 +0530

    APril issue updated

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,7 +1,7 @@
 <div class="container dynamic-page">
   <div class="row justify-content-center">
     <div class="col-md-12 gap-above-med">
-      <h1>ಪ್ರಸ್ತುತ ಸಂಚಿಕೆ: ಮಾರ್ಚ್</h1>
+      <h1>ಸಂಗ್ರಹ > ಪರಿವಿಡಿ > ಸಂಪುಟ ೨೦, ಸಂಚಿಕೆ ೪</h1>
     </div>
   </div>
   <div class="row justify-content-center">
@@ -10,22 +10,22 @@
         <div class="col-md-5 mainArticle">
           <div class="row">
             <div col-md-12>
-              <a href="<?=BASE_URL?>article/text/020/03/#page=24" target="_blank">
-                <img class="img-fluid" src="<?=DATA_URL?>020/03/0024-0028.jpg" />
-                <p class="title">ಆನಂದದ ಹುಡುಕಾಟ - ೩</p>
-                <p class="content">ಭಗವಾನ್ ಬುದ್ಧನ ಉಪದೇಶವು ದುಃಖದ ಸತ್ಯತೆ ಮತ್ತು ಅದರಿಂದ ಪಾರಾಗುವ ಉಪಾಯವನ್ನು ಆಧರಿಸಿದೆ. ಅವನ ಪ್ರಕಾರ ರೋಗ, ಜರೆ, ಮೃತ್ಯು – ಇವೆಲ್ಲವೂ ....</p>
-                <span>
-                  <a class="author by" href="<?=BASE_URL?>articles/author/ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ">ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ</a>
-                </span>
+              <a href="<?=BASE_URL?>article/text/020/04/#page=7" target="_blank">
+                <img class="img-fluid" src="<?=DATA_URL?>020/04/0007-0009.jpg">
+                <p class="title">ಪ್ರಪಂಚ, ಪರಮಾರ್ಥ</p>
+                <p class="content">ಎಲ್ಲ ಜೀವಿಗಳಂತೆ ಮಾನವನೂ ಈ ಪ್ರಪಂಚದಲ್ಲೇ ಹುಟ್ಟಿ ಬೆಳೆದಿದ್ದಾನೆ; ಇಲ್ಲೇ ಉಂಡು, ತಿಂದು, ನಿದ್ರಿಸಿ, ಓಡಾಡುತ್ತ ಜೀವನ ನಡೆಸುತ್ತಿದ್ದಾನೆ. ಭೂಮಿಯನ್ನು...</p>
               </a>
             </div>
           </div>
           <div class="row">
             <div col-md-12>
-              <a href="<?=BASE_URL?>article/text/020/03/#page=7" target="_blank">
-                <img class="img-fluid" src="<?=DATA_URL?>020/03/0007-0009.jpg" />
-                <p class="title">ಸಂನ್ಯಾಸ ಮತ್ತು ಗೃಹಸ್ಥ ಜೀವನ</p>
-                <p class="content">ಪ್ರಪಂಚದ ಪ್ರಮುಖ ಧರ್ಮಗಳೆಲ ಸಂನ್ಯಾಸ ಜೀವನಕೆ ವವರು ಅಸಾಧಾರಣ ಮನೋಬಲ, ವೈರಾಗ್ಯ ಹಾಗೂ ನಿಶ್ಚಿತ ಮಹತ್ವ ಕೊಟ್ಟಿರುವುದನ್ನು ನಾವು ...</p>
+              <a href="<?=BASE_URL?>article/text/020/04/#page=24" target="_blank">
+                <img class="img-fluid" src="<?=DATA_URL?>020/04/0024-0027.jpg">
+                <p class="title">ಆನಂದದ ಹುಡುಕಾಟ - ೪</p>
+                <p class="content">ಪಾಂಡವರು ೧೮ ವರ್ಷ ವನವಾಸದಲ್ಲಿದಾಗ ಒಂದು ಘಟನೆ ನಡೆಯಿತು. ಒಂದು ದಿನ ಕಾಡಿನಲ್ಲಿ ಸಂಚರಿಸುತಿದ ಪಾಂಡವರು ಮತ್ತು ದ್ರೌಪದಿಗೆ ಬಹಳ...</p>
+                <span>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ">ಬ್ರಹ್ಮಶಾನಂದ, ಸ್ವಾಮಿ</a>
+                </span>
               </a>
             </div>
           </div>
@@ -33,27 +33,36 @@
         <div class="col-md-3 subArticle">
           <div class="row">
             <div col-md-12>
-              <a href="<?=BASE_URL?>article/text/020/03/#page=11" target="_blank">
-                <img class="img-fluid" src="<?=DATA_URL?>020/03/0011-0014.jpg" />
-                <p class="title">ಶ್ರೀಮಾತೆಯವರೊಂದಿಗೆ - ಭಾಗ ೧</p>
-                <p class="content">ಶ್ರೀಮಾತೆಯವರ ಶಿಷ್ಯರೂ, ಅವರ ಆತ್ಮೀಯ ಸೇವಕರೂ ಆಗಿದ್ದ ಲೇಖಕರು ರಾಮಕೃಷ್ಣ ...</p>
+              <a href="<?=BASE_URL?>article/text/020/04/#page=15" target="_blank">
+                <img class="img-fluid" src="<?=DATA_URL?>020/04/0015-0018.jpg">
+                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೭</p>
+                <p class="content">ಸಾಮೀಜಿಯವರ ದೇಹತ್ಯಾಗದಿಂದ ವಿರಜಾನಂದರ ದೇಹಮನಸ್ಸುಗಳು ಕಮರಿಹೋಗಿದ್ದ ಸಂದರ್ಭ...</p>
                 <span>
-                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ">ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
-                </span>
-                <span>
-                  <a class="author by" href="<?=BASE_URL?>articles/author/ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ">ಜ್ಞಾನಯೋಗಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
               </a>
             </div>
           </div>
           <div class="row">
             <div col-md-12>
-              <a href="<?=BASE_URL?>article/text/020/03/#page=15" target="_blank">
-                <img class="img-fluid" src="<?=DATA_URL?>020/03/0015-0017.jpg" />
-                <p class="title">ಕಳೆದ ದಿನಗಳ ನೆನಪುಗಳು - ೨೬</p>
-                <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಸಂನ್ಯಾಸಿ ಶಿಷ್ಯರಲ್ಲೊಬ್ಬರಾದ ಸ್ವಾಮಿ ಅದ್ದುತಾ ಕೊಂಡರು ನಂದರು ೧೯೨೦ರ ಏಪ್ರಿಲ್ ...</p>
+              <a href="<?=BASE_URL?>article/text/020/04/#page=19" target="_blank">
+                <img class="img-fluid" src="<?=DATA_URL?>020/04/0019-0020.jpg">
+                <p class="title">ಅಧ್ಯಾತ್ಮ ಚಿಂತನೆ</p>
+                <p class="content">ಮಾನವನು ಸಸ್ಯದಂತೆ ಹುಟ್ಟಿ, ಬದುಕಿ, ಜೀವನ ನಡೆಸಿ ಸತ್ತರೆ ಅದರಿಂದ ಸಾರ್ಥಕವೇನು? ಅವನ...</p>
                 <span>
-                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ">ಶ್ರದ್ಧಾನಂದ, ಸ್ವಾಮಿ</a>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಕೃಷ್ಣಮೂರ್ತಿ ಕೆ. ಎಸ್.">ಕೃಷ್ಣಮೂರ್ತಿ ಕೆ. ಎಸ್.</a>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="row">
+            <div col-md-12>
+              <a href="<?=BASE_URL?>article/text/020/04/#page=32" target="_blank">
+                <img class="img-fluid" src="<?=DATA_URL?>020/04/0032-0033.jpg">
+                <p class="title">'ನಿನ್ನ ಏಳಿಗೆಗೆ ನೀನೇ ಶಿಲ್ಪಿ'</p>
+                <p class="content">ಒಂದು ಸತ್ಯಕಥೆಯನ್ನಾಧರಿಸಿ ಸ್ವಾಮೀಜಿಯವರ ವಿಚಾರ ವನ್ನು ಈ ಲೇಖನದಲ್ಲಿ ಮನದಟ್ಟು...</p>
+                <span>
+                  <a class="author by" href="<?=BASE_URL?>articles/author/ಶಾಂತಿವ್ರತಾನಂದ, ಸ್ವಾಮಿ">ಶಾಂತಿವ್ರತಾನಂದ, ಸ್ವಾಮಿ</a>
                 </span>
               </a>
             </div>
@@ -61,65 +70,90 @@
         </div>
         <div class="col-md-4 cardArticle">
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=4" target="_blank">
-              <img class="img-fluid" src="<?=DATA_URL?>020/03/0004-0006.jpg" />
-              <p class="title">ಶ್ರೀರಾಮಕೃಷ್ಣರ ಭಜನೆ</p>
-              <p class="content">ಅರೂಪ ಸಾಯರೆ ಲೀಲಾ ಲಹರಿ ಉಠಿಲೊ ಮೃದುಲ ಕರುಣಾಬಾಯ್ ಆದಿ ಅಂತಹೀನ ಅಖಂಡೇ ವಿಲೀನ, ಮಾಯಾಯ್ ಥ್ರಿಲೇ...</p>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=10" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0010-0010.jpg">
+              <p class="title">ನಿವೃತ್ತಿಮಾರ್ಗ</p>
+              <p class="content">ಮನಸಿನಲ್ಲಿ ನಿವೃತ್ತಿಯುಂಟಾಯಿತು ಎಂದರೆ, ವಿವೇಕ ಉದಯವಾಗುತ್ತದೆ. ವಿವೇಕ ಉದಯವಾಯಿತು ಎಂದರೆ, ತಾತ್ತಿ ಕ ಭಾವನೆ ...</p>
+            </a>
+          </div>
+          <div class="cards-wrapper">
+            <a href="<?=BASE_URL?>article/text/020/04/#page=21" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0021-0021.jpg">
+              <p class="title">ಮಮಕಾರವೆಂಬ ಹಗ್ಗ !</p>
+              <p class="content">ಒಕ್ಕಲಿಗ ಮುಂದೆ ಮುಂದೆ ಹೊರಟಿರುವನು. ಅದರಿಂದ ಕಟ್ಟಲ್ಪಟ್ಟ ಎತ್ತು ಹಿಂದೆ-ಹಿಂದೆ...</p>
               <span>
-                <a class="author by" href="<?=BASE_URL?>articles/author/ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ">ಪ್ರೇಮೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ತೀರ್ಥಕರಾನಂದ, ಸ್ವಾಮಿ">ತೀರ್ಥಕರಾನಂದ, ಸ್ವಾಮಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=10" target="_blank">
-              <img class="img-fluid" src="<?=DATA_URL?>020/03/0010-0010.jpg" />
-              <p class="title">ಆಧ್ಯಾತ್ಮಿಕ ಜೀವನದ ಸಾರ</p>
-              <p class="content">ನಿಮ್ಮ ಚೀಲ' ಎಂಬುದಾಗಿ ಆ ನೀರು ಹೊರುವ ತಿನ್ನುವುದು, ತಂಬಾಕು ಸೇವಿಸುವುದು, ಮೈಗೆ ವ್ಯಕ್ತಿ ಹೇಳಿದುದರ ಅರ್ಥ ದೇಹ, ಹೊಟ್ಟೆ ಇತ್ಯಾದಿ....</p>
-              <span>
-                <a class="author by" href="<?=BASE_URL?>articles/author/ವಚನವೇದದಿಂದ">ವಚನವೇದದಿಂದ</a>
-              </span>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=28" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0028-0028.jpg">
+              <p class="title">ಜಯಲಕ್ಷ್ಮಿ ಸಾರಾಬಾಯಿ ಮತ್ತು ಕೇಶರ್ ಬಾಯಿ</p>
+              <p class="content">ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರು ಪಾಲಿತಾನಕ್ಕೆ ಭೇಟಿಯಿತ್ತಾಗ ದಿವಾನ್ ಚುನಿಲಾಲ್ ಸಾರಾಬಾಯಿ ಹಜರತ್ರ ಮನೆಯಲ್ಲಿ ತಂಗಿದ್ದರು. ವಿವಾಹವಾಗಿ ...</p>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=29" target="_blank">
-              <img class="img-fluid" src="<?=DATA_URL?>020/03/0029-0032.jpg" />
-              <p class="title">ಶರತ್‌ಚಂದ್ರ ಚಕ್ರವರ್ತಿ - ಭಾಗ ೧</p>
-              <p class="content">ದಿವ್ಯತೆಯ ಸರ್ಶದಿಂದ ಅತ್ಯಂತ ಸಾಮಾನ್ಯ ವ್ಯಕ್ತಿಯೂ ಅತಿಶ್ರೇಷ್ಠ ಪದವಿಯನ್ನು ಹೊಂದುವನು ಎನ್ನುವ ಮಾತಿಗೆ ಉತ್ತಮ ನಿದರ್ಶನವೇ ...</p>
-              <span>
-                <a class="author by" href="<?=BASE_URL?>articles/author/ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ">ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ</a>
-              </span>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=34" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0034-0034.jpg">
+              <p class="title">ಯುವಜನತೆಗೆ ಅತ್ಯುತ್ತಮ ಗ್ರಂಥಗಳು</p>
+              <p class="content">“ನನ್ನ ಭರವಸೆಯೆಲ್ಲ ಈಗಿನ ಯುವಕರ ಮೇಲೆ ನಿಂತಿದೆ' ಎಂದಿದ್ದರು ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರು. ಅವರ ಆಶಯದಂತೆ, ಭವಿಷ್ಯ ಭಾರತದ ಸುಭದ್ರ...</p>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=38" target="_blank">
-              <img class="img-fluid" src="<?=DATA_URL?>020/03/0038-0041.jpg" />
-              <p class="title">ಕೃಪೆ ತಂದ ವಿಜಯ</p>
-              <p class="content">ಶ್ರೀರಾಮಕೃಷ್ಣರು ಆಗ ದಕ್ಷಿಣೇಶ್ವರದಲ್ಲಿ ವಾಸವಾಗಿದ್ದರು. ಒಂದು ದಿನ ಮಧುರ ಬಾಬುವಿನ ಅರಮನೆಯಲ್ಲಿ ಸ್ವಾಮಿ, ನಮ್ಮ ದೇವಾಲಯದ...</p>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=38" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0038-0041.jpg">
+              <p class="title">ಶಿಷ್ಯಂದಿರ ತರಬೇತಿ</p>
+              <p class="content">ಶ್ರೀ ರಾಮಕೃಷ್ಣರು ತಮ್ಮ ಶಿಷ್ಯಂದಿರಿಗೆ ಅವರವರ ಸ್ವಭಾವಕ್ಕೆ ತಕ್ಕಂತೆ ತರಬೇತಿ ನೀಡುವುದರಲ್ಲಿ...</p>
               <span>
                 <a class="author by" href="<?=BASE_URL?>articles/author/ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ">ವಿಮೂರ್ತಾನಂದ, ಸ್ವಾಮಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=42" target="_blank">
-              <img class="img-fluid" src="<?=DATA_URL?>020/03/0042-0042.jpg" />
-              <p class="title">ಮೈಸೂರು ಆಶ್ರಮದ ಕಾರ್ಯಕ್ರಮಗಳು</p>
-              <p class="content">'ವಿವೇಕ ಶಿಕ್ಷಣ'ದ ವಾರ್ಷಿಕೋತ್ಸವ : ಮೈಸೂರು ಶ್ರೀರಾಮಕೃಷ್ಣ ಆಶ್ರಮವು ಕಳೆದ ೧೯ ವರ್ಷಗಳಿಂದ ನಡೆಸಿಕೊಂಡು ಬರುತ್ತಿರುವ, ಆರ್ಥಿಕ ಹಾಗೂ...</p>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=42" target="_blank">
+              <img class="img-fluid" src="<?=DATA_URL?>020/04/0042-0044.jpg">
+              <p class="title">ಬೆಳಗಾವಿ ಆಶ್ರಮದಲ್ಲಿ 'ವಿವೇಕಾನಂದ...</p>
+              <p class="content">ಬೆಳಗಾವಿ ರಾಮಕೃಷ್ಣ ಮಿಷನ್ನಿನಲ್ಲಿ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದ ಸ್ಮಾರಕ'ದ ಉದ್ಘಾಟನೆ ಸ್ವಾಮಿ ವಿವೇಕಾನಂದರು ತಮ್ಮ ಪರಿವ್ರಾಜಕ ದಿನಗಳಲ್ಲಿ, ಫೆಬ್ರವರಿ ೧ರಂದು ಈ ನವೀಕೃತ ನಿವಾಸ 'ಸ್ವಾಮಿ...</p>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=18" target="_blank">
-              <p class="title">ಅಪರೂಪದ ಗುರು ಅಲ್ಲಮಪ್ರಭು</p>
-              <p class="content">ಹನ್ನೆರಡನೇ ಶತಮಾನದಲ್ಲಿ ಶರಣ ಚಳವಳಿಯ ನೇತಾರರಲ್ಲಿ ದಿವ್ಯ ಜ್ಯೋತಿಯಾಗಿ ಬೆಳಗಿದ ಶ್ರೇಷ್ಠ ಸಂತರೇ ಅಲ್ಲಮ ಪ್ರಭು ದೇವರು. ಬಸವೇಶ್ವರರೇ ಮೊದಲಾದ ಮಹಾನ್ ಶರಣರ ಒಡನಾಡಿಯೂ...</p>
+            <a href="<?=BASE_URL?>article/text/020/04/#page=4" target="_blank">
+              <p class="title">ವಿಶ್ವಶಾಂತಿ ಪ್ರಾರ್ಥನೆ</p>
+              <p class="content">ಎಲ್ಲರಿಗೂ ಒಳ್ಳೆಯದಾಗಲಿ, ರಾಜರುಗಳು ನ್ಯಾಯ ಮಾರ್ಗ ದಿಂದ ಭೂಮಿಯನ್ನು ಆಳುವಂತಾಗಲಿ. ಎಲ್ಲ ಜೀವಿಗಳಿಗೂ ಶುಭವುಂಟಾಗಲಿ, ಇಡೀ ಜಗತ್ತಿಗೆ ಸುಖವುಂಟಾಗಲಿ ಸರ್ವಸ್ತರತು ದುರ್ಗಾಣಿ ಸರ್ವೊ ಭದ್ರಾಣಿ ಪಶ್ಯತು...</p>
+            </a>
+          </div>
+          <div class="cards-wrapper">
+            <a href="<?=BASE_URL?>article/text/020/04/#page=11" target="_blank">
+              <p class="title">ಶ್ರೀಮಾತೆಯವರೊಂದಿಗೆ - ಭಾಗ ೨</p>
+              <p class="content">ಜಯರಾಮಬಾಟಿಯಲಿ ಶ್ರೀಮಾತೆಯವರ ಮನೆಯಲ್ಲಿನ ಊಟವೆಂದರೆ ಮಧ್ಯಮವರ್ಗದ ಕುಟುಂಬದ ಊಟದಂತೆ ಬೆಳಗಿನ ಹೊತ್ತು...</p>
               <span>
-                <a class="author by" href="<?=BASE_URL?>articles/author/ಶಶಿಕಲಾ ಶ್ರೀಧರ್">ಶಶಿಕಲಾ ಶ್ರೀಧರ್</a>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ">ಶಾರದೇಶಾನಂದ, ಸ್ವಾಮಿ</a>
               </span>
             </a>
           </div>
           <div class="cards-wrapper">
-            <a href="<?=BASE_URL?>article/text/020/03/#page=33" target="_blank">
+            <a href="<?=BASE_URL?>article/text/020/04/#page=22" target="_blank">
+              <p class="title">ಶ್ರೀಮಾತೆಯವರ ಪತ್ರಗಳು - ೧೭</p>
+              <p class="content">ತಾಯಿ, ನಿನ್ನ ಪತ್ರವು ಸಿಕ್ಕಿದೆ. ನಾನು ಸದ್ಯ ಚೆನ್ನಾಗಿದ್ದೇನೆ. ಆದರೆ ಪ್ರತಿದಿನ ಸ್ನಾನ ಮಾಡಿದರೆ ಕಿವಿಯು ಸಲ ಭಾರವಾಗು ವುದು. ರಾಧು ಸಾಧಾರಣ..</p>
+              <span>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಧರ್ಮಪ್ರಾಣಾ, ಪ್ರವ್ರಾಜಿಕಾ">ಧರ್ಮಪ್ರಾಣಾ, ಪ್ರವ್ರಾಜಿಕಾ</a>
+              </span>
+            </a>
+          </div>
+          <div class="cards-wrapper">
+            <a href="<?=BASE_URL?>article/text/020/04/#page=29" target="_blank">
+              <p class="title">ಶರತ್‌ಚಂದ್ರ ಚಕ್ರವರ್ತಿ - ಭಾಗ ೨</p>
+              <p class="content">ಒಂದು ದಿನ ಬಟಾಚಾರ್ಯನು ಸಾಮೀಜಿಯವರೊಡನೆ ತ್ತಿದ್ದೇನೆ ನೀಲಾಂಬರ ಮುಖರ್ಜಿ ತೋಟದ ಮನೆಯಲ್ಲಿ ರಾತ್ರಿ ತಂಗಿದನು. ಸ್ವಾಮೀಜಿ ಸಾಕ್ಷಾತ್ ಶಿವ ಎಂಬುದಾಗಿ ಸ್ವಪ್ನವಾಗಿತ್ತು ಶಿಷ್ಯನಿಗೆ...</p>
+              <span>
+                <a class="author by" href="<?=BASE_URL?>articles/author/ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ">ಛಾಯಾ ಬ್ಯಾನರ್ಜಿ</a>
+              </span>
+            </a>
+          </div>
+          <div class="cards-wrapper">
+            <a href="<?=BASE_URL?>article/text/020/04/#page=35" target="_blank">
               <p class="title">ಸಾವಿನಾಚೆಗೆ, ಚೈತನ್ಯದ ಬೆಳಕಿಗೆ</p>
-              <p class="content">ಸ್ವಾಮಿ ವೀರೇಶಾನಂದ (ಈ ನಾಟಕವನ್ನು ಕಠೋಪನಿಷತ್ತಿನ ಆಧಾರದ ಮೇಲೆ ರಚಿಸಲಾಗಿದೆ. ಕಠೋಪನಿಷತ್ತು ಉಪನಿಷತ್ತುಗಳಲ್ಲೆಲ್ಲ ಪ್ರಸಿದ್ಧವಾದುದು. ಇಲ್ಲಿ ಬಾಲಕ ನಚಿಕೇತನಿಗೆ ಮೃತ್ಯುದೇವತೆಯಾದ...</p>
+              <p class="content">ಯಮ : ಮಗು, ವಿದ್ಯೆ, ಅವಿದ್ಯೆ ಇವೆರಡರ ನಡುವಿನ ವ್ಯತ್ಯಾಸವನ್ನು ನೀನು ತಿಳಿದುಕೊಳ್ಳಬೇಕಪ್ಪಾ. ಇವೆರಡೂ ವಿರುದ್ದ ವಾದುವು. ದೂರ ದೂರವಾಗಿ ಇರು ವಂಥವು. ಮಗೂ, ನಚಿಕೇತ, ನೀನು ಅವಿದ್ಯೆಯನ್ನು ತಿರಸ್ಕರಿಸಿದ್ದೀಯೆ. ಯಾವ ಕಾಮನೆಗಳೂ ನಿನ್ನನ್ನು ಆಕರ್ಷಿಸಲಿಲ್ಲ...</p>
             </a>
           </div>
         </div>
